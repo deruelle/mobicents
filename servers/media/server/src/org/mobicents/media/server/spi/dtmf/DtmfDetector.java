7f2b38776549fad7f538bb918f74871f69c7e470
oleg.kulikoff <oleg.kulikoff@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2008-01-22 10:09:39 +0000

    Initial implementation
    git-svn-id: http://mobicents.googlecode.com/svn/trunk@233 bf0df8d0-2c1f-0410-b170-bd30377b63dc

---
+++ servers/media/server/src/org/mobicents/media/server/spi/dtmf/DtmfDetector.java
@@ -0,0 +1,26 @@
+/*
+ * Mobicents Media Gateway
+ *
+ * The source code contained in this file is in in the public domain.
+ * It can be used in any project or product without prior permission,
+ * license or royalty payments. There is  NO WARRANTY OF ANY KIND,
+ * EXPRESS, IMPLIED OR STATUTORY, INCLUDING, WITHOUT LIMITATION,
+ * THE IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE,
+ * AND DATA ACCURACY.  We do not warrant or make any representations
+ * regarding the use of the software or the  results thereof, including
+ * but not limited to the correctness, accuracy, reliability or
+ * usefulness of the software.
+ */
+
+package org.mobicents.media.server.spi.dtmf;
+
+/**
+ *
+ * @author Oleg Kulikov
+ */
+public interface DtmfDetector {
+    public final static int RFC2833 = 1;
+    public final static int INBOUND = 2;
+    
+    public void setDtmfMask(String regExp);
+}
