7cbd532526a84ed8529e910432823acb59d2181a
amit.bhayani <amit.bhayani@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2008-03-09 11:11:05 +0000

    Adding MediaServerManagementMBean that logs the version of Media Server
    git-svn-id: http://mobicents.googlecode.com/svn/trunk@382 bf0df8d0-2c1f-0410-b170-bd30377b63dc

---
+++ servers/media/server/src/org/mobicents/media/server/impl/jmx/MediaServerManagement.java
@@ -0,0 +1,86 @@
+package org.mobicents.media.server.impl.jmx;
+
+import javax.management.ObjectName;
+
+import org.jboss.logging.Logger;
+import org.jboss.system.ServiceMBeanSupport;
+import org.mobicents.media.server.impl.Version;
+
+/**
+ * 
+ * @author amit.bhayani
+ * 
+ */
+
+// TODO Handle option to Start, Stop MediaServer gracefully
+public class MediaServerManagement extends ServiceMBeanSupport implements
+		MediaServerManagementMBean {
+
+	private ObjectName annTrunkManagementMBean;
+	private ObjectName confEndpointManagementMBean;
+	private ObjectName loopEndpointManagementMBean;
+	private ObjectName prTrunkManagementMBean;
+
+	public MediaServerManagement() {
+		super(MediaServerManagementMBean.class);
+		logger.info("[[[[[[[[[ " + getVersion() + " starting... ]]]]]]]]]");
+	}
+
+	private static Logger logger = Logger
+			.getLogger(MediaServerManagement.class);
+
+	@Override
+	protected void startService() throws Exception {
+		super.startService();
+		logger.info("[[[[[[[[[ " + getVersion() + " Started " + "]]]]]]]]]");
+	}
+
+	@Override
+	protected void stopService() throws Exception {
+		super.stopService();
+		logger.info("[[[[[[[[[ " + getVersion() + " Stopped " + "]]]]]]]]]");
+	}
+
+	public ObjectName getAnnTrunkManagementMBean() {
+		return this.annTrunkManagementMBean;
+	}
+
+	public ObjectName getConfEndpointManagementMBean() {
+		return this.confEndpointManagementMBean;
+	}
+
+	public ObjectName getLoopEndpointManagementMBean() {
+		return this.loopEndpointManagementMBean;
+	}
+
+	public ObjectName getPRTrunkManagementMBean() {
+		return this.prTrunkManagementMBean;
+	}
+
+	public String getVersion() {
+		return Version.instance.toString();
+	}
+
+	public void setAnnTrunkManagementMBean(ObjectName annTrunkManagementMBean) {
+		this.annTrunkManagementMBean = annTrunkManagementMBean;
+
+	}
+
+	public void setConfEndpointManagementMBean(
+			ObjectName confEndpointManagementMBean) {
+		this.confEndpointManagementMBean = confEndpointManagementMBean;
+
+	}
+
+	public void setLoopEndpointManagementMBean(
+			ObjectName loopEndpointManagementMBean) {
+		this.loopEndpointManagementMBean = loopEndpointManagementMBean;
+
+	}
+
+	public void setPRTrunkManagementMBean(ObjectName prTrunkManagementMBean) {
+		this.prTrunkManagementMBean = prTrunkManagementMBean;
+
+	}
+
+}
