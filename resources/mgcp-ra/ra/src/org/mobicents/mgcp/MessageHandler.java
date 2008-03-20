af82ee0928c23a570ef871deaefca9f6d0787381
oleg.kulikoff <oleg.kulikoff@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2008-03-20 10:38:21 +0000

    Issue #169671 : RestartInProgress message implementation
    git-svn-id: http://mobicents.googlecode.com/svn/trunk@472 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- resources/mgcp-ra/ra/src/org/mobicents/mgcp/MessageHandler.java
+++ resources/mgcp-ra/ra/src/org/mobicents/mgcp/MessageHandler.java
@@ -13,7 +13,6 @@
  * but not limited to the correctness, accuracy, reliability or
  * usefulness of the software.
  */
-
 package org.mobicents.mgcp;
 
 import java.net.InetAddress;
@@ -23,40 +22,39 @@ import org.apache.log4j.Logger;
 /**
  *
  * @author Oleg Kulikov
- * @author Pavel Mitrenko
  */
 public class MessageHandler implements Runnable {
-    
+
     private JainMgcpStackImpl stack;
     private byte[] data;
     private InetAddress address;
     private int port;
     private Logger logger = Logger.getLogger(MessageHandler.class);
-    
+
     /** Creates a new instance of MessageHandler */
-    public MessageHandler(JainMgcpStackImpl stack, byte[] data, 
+    public MessageHandler(JainMgcpStackImpl stack, byte[] data,
             InetAddress address, int port) {
         this.stack = stack;
         this.data = data;
         this.address = address;
         this.port = port;
     }
-    
+
     public boolean isRequest(String header) {
         return header.matches("[\\w]{4}(\\s|\\S)*");
     }
-    
-    public void run() {        
+
+    public void run() {
         String msg = new String(data);
         int pos = msg.indexOf("\n");
-        
+
         // extract message header to determine transaction handle parameter
         // and type of the received message
         String header = msg.substring(0, pos).trim();
         if (logger.isDebugEnabled()) {
             logger.debug("Message header: " + header);
         }
-        
+
         // check message type
         // if this message is command then create new transaction handler
         // for specified type of this message.
@@ -64,11 +62,11 @@ public class MessageHandler implements Runnable {
         // transaction to handle this message
         String tokens[] = header.split("\\s");
         if (isRequest(header)) {
-            
-        	if (logger.isDebugEnabled()) {
+
+            if (logger.isDebugEnabled()) {
                 logger.debug("Processing command message");
             }
-        	
+
             String verb = tokens[0];
             if (verb.equalsIgnoreCase("crcx")) {
                 TransactionHandler handle = new CreateConnectionHandler(
@@ -94,31 +92,33 @@ public class MessageHandler implements Runnable {
                 TransactionHandler handle = new NotifyHandler(
                         stack, address, port);
                 handle.receiveCommand(msg);
-            }
-            else {
+            } else if (verb.equalsIgnoreCase("rsip")) {
+                TransactionHandler handle = new RestartInProgressHandler(
+                        stack, address, port);
+                handle.receiveCommand(msg);
+            } else {
                 logger.warn("Unsupported message verbose " + verb);
                 return;
             }
-            
+
         } else {
-        	
-        	// RESPONSE HANDLING
-        	
+
+            // RESPONSE HANDLING
+
             if (logger.isDebugEnabled()) {
                 logger.debug("Processing response message");
             }
-            
+
             Integer tid = Integer.decode(tokens[1]);
-            
+
             TransactionHandler handler = (TransactionHandler) stack.transactions.remove(tid);
-            
+
             if (handler == null) {
                 logger.warn("Unknown transaction :" + tid);
                 return;
             }
-            
+
             handler.receiveResponse(msg);
         }
     }
-    
 }
