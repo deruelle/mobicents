b1f71ff717b0b9e42a172c8f7163f689756a3bcf
oleg.kulikoff <oleg.kulikoff@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2008-04-23 08:28:48 +0000

    Reduce buffer upto 0.5 sec. Calculate relative threshold
    git-svn-id: http://mobicents.googlecode.com/svn/trunk@1448 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- servers/media/jar/src/main/java/org/mobicents/media/server/impl/dtmf/InbandDetector.java
+++ servers/media/jar/src/main/java/org/mobicents/media/server/impl/dtmf/InbandDetector.java
@@ -31,162 +31,211 @@ import org.mobicents.media.protocol.PushBufferStream;
 import org.mobicents.media.server.impl.common.MediaResourceState;
 import org.mobicents.media.server.impl.jmf.dsp.Codec;
 import org.mobicents.media.server.impl.jmf.dsp.CodecLocator;
-import org.mobicents.media.server.impl.jmx.MediaServerManagementMBean;
+import org.mobicents.media.server.impl.common.MediaResourceState;
 
 /**
  * 
  * @author Oleg Kulikov
  */
 public class InbandDetector extends BaseDtmfDetector implements
-		BufferTransferHandler {
+        BufferTransferHandler {
 
-	public final static String[][] events = new String[][] {
-			{ "1", "2", "3", "A" }, { "4", "5", "6", "B" },
-			{ "7", "8", "9", "C" }, { "*", "0", "#", "D" } };
+    public final static String[][] events = new String[][]{
+        {"1", "2", "3", "A"}, {"4", "5", "6", "B"},
+        {"7", "8", "9", "C"}, {"*", "0", "#", "D"}};
+    
+    private int[] lowFreq = new int[]{697, 770, 852, 941};
+    private int[] highFreq = new int[]{1209, 1336, 1477, 1633};
+    private Codec codec;
+    private byte[] localBuffer = new byte[16000];
+    private int offset = 0;
+    private Filter filter = new Filter(10);
+    private double threshold = 10;
+    
+    private Logger logger = Logger.getLogger(InbandDetector.class);
 
-	private int[] lowFreq = new int[] { 697, 770, 852, 941 };
-	private int[] highFreq = new int[] { 1209, 1336, 1477, 1633 };
-	private Codec codec;
-	private byte[] localBuffer = new byte[16000];
-	private int offset = 0;
-	private Filter filter = new Filter(10);
-	private Logger logger = Logger.getLogger(InbandDetector.class);
+    public InbandDetector() {
+        super();
+        //Properties props = new Properties();
+        try {
+//            props.load(getClass().getResourceAsStream("dtmf.properties"));
+//            int t = Integer.parseInt(props.getProperty("dtmf.threshold"));
+            filter = new Filter();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	public InbandDetector() {
-		super();
+    public void configure(Properties config) {
+        setState(MediaResourceState.CONFIGURED);
+    }
 
-		try {
-			MBeanServer server = MBeanServerLocator.locateJBoss();
-			ObjectName objectName = new ObjectName(
-					"media.mobicents:service=MediaServerManagement");
+    public void prepare(PushBufferStream stream)
+            throws UnsupportedFormatException {
+        stream.setTransferHandler(this);
+        if (!stream.getFormat().matches(Codec.LINEAR_AUDIO)) {
+            codec = CodecLocator.getCodec(stream.getFormat(),
+                    Codec.LINEAR_AUDIO);
+            setState(MediaResourceState.PREPARED);
+            if (codec == null) {
+                throw new UnsupportedFormatException(stream.getFormat());
+            }
+        }
+    }
 
-			MediaServerManagementMBean mbean = (MediaServerManagementMBean) MBeanServerInvocationHandler
-					.newProxyInstance(server, objectName,
-							MediaServerManagementMBean.class, false);
+    public void start() {
+        setState(MediaResourceState.STARTED);
+    }
 
-			int dtmfThreshold = mbean.getDtmfThreshold();
+    public void stop() {
+        setState(MediaResourceState.PREPARED);
+    }
 
-			filter = new Filter(dtmfThreshold);
+    public void transferData(PushBufferStream stream) {
+        if (getState() != MediaResourceState.STARTED) {
+            return;
+        }
 
-		} catch (MalformedObjectNameException e1) {
-			logger
-					.error(
-							"The media.mobicents:service=MediaServerManagement MBean not found",
-							e1);
-		} catch (NullPointerException e1) {
-			logger.error("Failed to create InbandDetector()", e1);
-		}
-	}
+        Buffer buffer = new Buffer();
+        try {
+            stream.read(buffer);
+        } catch (IOException e) {
+        }
 
-	public void configure(Properties config) {
-		setState(MediaResourceState.CONFIGURED);
-	}
+        byte[] data = (byte[]) buffer.getData();
 
-	public void prepare(PushBufferStream stream)
-			throws UnsupportedFormatException {
-		stream.setTransferHandler(this);
-		if (!stream.getFormat().matches(Codec.LINEAR_AUDIO)) {
-			codec = CodecLocator.getCodec(stream.getFormat(),
-					Codec.LINEAR_AUDIO);
-			setState(MediaResourceState.PREPARED);
-			if (codec == null) {
-				throw new UnsupportedFormatException(stream.getFormat());
-			}
-		}
-	}
+        if (codec != null) {
+            data = codec.process(data);
+        }
 
-	public void start() {
-		setState(MediaResourceState.STARTED);
-	}
+        int len = Math.min(8000/*16000*/ - offset, data.length);
+        System.arraycopy(data, 0, localBuffer, offset, len);
+        offset += len;
 
-	public void stop() {
-		setState(MediaResourceState.PREPARED);
-	}
+        if (logger.isDebugEnabled()) {
+            logger.debug("append " + len + " bytes to the local buffer, buff size=" + offset);
+        }
 
-	public void transferData(PushBufferStream stream) {
-		if (getState() != MediaResourceState.STARTED) {
-			return;
-		}
+        // buffer full?
+        if (offset == 8000/*16000*/) {
+            double[] signal = new double[4000/*8000*/];
+            int k = 0;
+            for (int i = 0; i < signal.length; i++) {
+                signal[i] = ((localBuffer[k++] & 0xff) | (localBuffer[k++] << 8));
+            }
 
-		Buffer buffer = new Buffer();
-		try {
-			stream.read(buffer);
-		} catch (IOException e) {
-		}
+            localBuffer = new byte[8000/*16000*/];
+            offset = 0;
 
-		byte[] data = (byte[]) buffer.getData();
-		// System.out.println("receive " + data.length + " bytes");
-		// InbandGenerator.print(data);
+            double p[] = getPower(lowFreq, signal);
+//            System.out.println("LOW----------------------");
+//            print(p);
+            
+//            System.out.println("HIGH----------------------");
+            double P[] = getPower(highFreq, signal);
+//            print(P);
+            String tone = getTone(p, P);
+            if (tone != null) {
+                digitBuffer.push(tone);
+            }
+        }
+    }
 
-		// byte[] data = null;
-		if (codec != null) {
-			data = codec.process(data);
-			// System.out.println("Decompressed to " + data.length + " bytes");
-			// InbandGenerator.print(data);
-		}
+    private void print(double p[]) {
+        for (int i = 0; i < p.length; i++) {
+            System.out.print(p[i] + " ");
+        }
+        System.out.println();
+    }
+    /**
+     * Calculate power of the specified frequencies.
+     * 
+     * @param freq the array of frequencies
+     * @param data signal
+     * @return the power for the respective frequency
+     */
+    private double[] getPower(int[] freq, double[] data) {
+        double[] power = new double[freq.length];
+        for (int i = 0; i < freq.length; i++) {
+            power[i] = filter.getPower(freq[i], data);
+        }
+        return power;
+    }
 
-		int len = Math.min(16000 - offset, data.length);
-		System.arraycopy(data, 0, localBuffer, offset, len);
-		offset += len;
+    /**
+     * Searches maximum value in the specified array.
+     * 
+     * @param data[] input data.
+     * @return the index of the maximum value in the data array.
+     */
+    private int getMax(double data[]) {
+        int idx = 0;
+        double max = data[0];
+        for (int i = 1; i < data.length; i++) {
+            if (max < data[i]) {
+                max = data[i];
+                idx = i;
+            }
+        }
+        return idx;
+    }
+    
+    private String getTone(double f[], double F[]) {
+        int fm = getMax(f);
+//        System.out.println("fm=" + fm);
+        boolean fd = true;
+        
+        double ft = threshold;
+        for (int i = 0; i < f.length; i++) {
+            if (fm == i) continue;
+            double r = f[fm]/(f[i] + 1E-15);
+//            System.out.println("r=" + r);
+            if (r < threshold) {
+                fd = false;
+                break;
+            } else if (r > ft) {
+                ft = r;
+            }
+        }
+        
+        ft = threshold + (ft - threshold)/2;
+//        System.out.println("++++ ft=" + ft);
+        
+        if (!fd) return null;
 
-		// System.out.println("append " + len + " bytes to the local buffer,
-		// buff size=" + offset);
-		if (logger.isDebugEnabled()) {
-			logger.debug("append " + len
-					+ " bytes to the local buffer, buff size=" + offset);
-		}
+        int Fm = getMax(F);
+//        System.out.println("Fm=" + Fm);
+        boolean Fd = true;
+        
+        for (int i = 0; i < F.length; i++) {
+            if (Fm == i) continue;
+            double r = F[Fm]/(F[i] + 1E-15);
+//            System.out.println("R=" + r);
+            if (r < threshold) {
+                Fd = false;
+                break;
+            }
+        }
+        
+        if (!Fd) return null;
+        
+        return events[fm][Fm];
+    }
+    
+    private int checkFreq(int[] freq, double[] data) {
+        for (int i = 0; i < freq.length; i++) {
+            if (logger.isDebugEnabled()) {
+                logger.debug("Checking freq: " + freq[i]);
+            }
+            if (filter.detect(freq[i], data)) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
-		// buffer full?
-		if (offset == 16000) {
-			// System.out.println("SIGNAL");
-			// InbandGenerator.print(localBuffer);
-			double[] signal = new double[8000];
-			int k = 0;
-			for (int i = 0; i < 8000; i++) {
-				signal[i] = ((localBuffer[k++] & 0xff) | (localBuffer[k++] << 8));
-				// signal[i] = signal[i] / Short.MAX_VALUE;
-				// System.out.println("s[" + i + "]=" +signal[i]);
-			}
-
-			localBuffer = new byte[16000];
-			offset = 0;
-
-			logger.debug("Checking low frequencies");
-			int f1 = checkFreq(lowFreq, signal);
-			// System.out.println("Low group: " + f1);
-			if (f1 == -1) {
-				logger.debug("No low frequencies were found, break");
-				return;
-			}
-
-			logger.debug("Found frequency " + lowFreq[f1]
-					+ " Checking hight frequencies");
-			int f2 = checkFreq(highFreq, signal);
-			if (f2 == -1) {
-				logger.debug("No high frequencies were found, break");
-				return;
-			}
-
-			logger.debug("Found frequency " + highFreq[f1] + ", evt="
-					+ events[f1][f2]);
-			digitBuffer.push(events[f1][f2]);
-		}
-	}
-
-	private int checkFreq(int[] freq, double[] data) {
-		for (int i = 0; i < freq.length; i++) {
-			if (logger.isDebugEnabled()) {
-				logger.debug("Checking freq: " + freq[i]);
-			}
-			if (filter.detect(freq[i], data)) {
-				return i;
-			}
-		}
-		return -1;
-	}
-
-	public void release() {
-		localBuffer = null;
-		setState(MediaResourceState.NULL);
-	}
+    public void release() {
+        localBuffer = null;
+        setState(MediaResourceState.NULL);
+    }
 }
