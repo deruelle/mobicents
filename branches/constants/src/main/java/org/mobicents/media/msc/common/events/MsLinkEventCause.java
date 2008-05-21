de6cdc70456eb638608bf3a0bbdf3915377eea4b
vladimir.ralev <vladimir.ralev@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2008-05-21 23:53:02 +0000

    Put it in the right place
    git-svn-id: http://mobicents.googlecode.com/svn/trunk@1677 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- branches/constants/src/main/java/org/mobicents/media/msc/common/events/MsLinkEventCause.java
+++
@@ -1,7 +0,0 @@
-package org.mobicents.media.msc.common.events;
-
-public enum MsLinkEventCause {
-
-NORMAL,FACILITY_FAILURE,ENDPOINT_UNKNOWN,RESOURCE_UNAVAILABLE,REMOTE_SDP_INVALID,REMOTE_SDP_MISSING;
-	
-}
--- branches/constants/src/main/java/org/mobicents/media/msc/common/events/MsLinkEventID.java
+++
@@ -1,5 +0,0 @@
-package org.mobicents.media.msc.common.events;
-
-public enum MsLinkEventID {
-	LINK_JOINED, LINK_DROPPED, LINK_FAILED;
-}
