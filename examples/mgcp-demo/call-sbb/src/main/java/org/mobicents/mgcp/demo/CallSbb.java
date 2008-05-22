6a2adab797c7a3f576d6b5596f2b05906a95a1d3
amit.bhayani@gmail.com <amit.bhayani@gmail.com@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2008-05-22 06:03:38 +0000

    Modified to create the ActivityContext for MGCP based on Tx ID
    git-svn-id: http://mobicents.googlecode.com/svn/trunk@1683 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- examples/mgcp-demo/call-sbb/src/main/java/org/mobicents/mgcp/demo/CallSbb.java
+++ examples/mgcp-demo/call-sbb/src/main/java/org/mobicents/mgcp/demo/CallSbb.java
@@ -20,10 +20,13 @@ import jain.protocol.ip.mgcp.message.DeleteConnection;
 import jain.protocol.ip.mgcp.message.parms.CallIdentifier;
 import jain.protocol.ip.mgcp.message.parms.ConflictingParameterException;
 import jain.protocol.ip.mgcp.message.parms.ConnectionDescriptor;
+import jain.protocol.ip.mgcp.message.parms.ConnectionIdentifier;
 import jain.protocol.ip.mgcp.message.parms.ConnectionMode;
 import jain.protocol.ip.mgcp.message.parms.EndpointIdentifier;
 import jain.protocol.ip.mgcp.message.parms.ReturnCode;
+
 import java.text.ParseException;
+
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sip.Dialog;
@@ -49,244 +52,274 @@ import javax.slee.RolledBackContext;
 import javax.slee.Sbb;
 import javax.slee.SbbContext;
 import javax.slee.UnrecognizedActivityException;
+
 import net.java.slee.resource.mgcp.JainMgcpProvider;
 import net.java.slee.resource.mgcp.MgcpActivityContextInterfaceFactory;
-import net.java.slee.resource.mgcp.MgcpEndpointActivity;
+import net.java.slee.resource.mgcp.MgcpConnectionActivity;
+
 import org.apache.log4j.Logger;
 import org.mobicents.slee.resource.sip.SipActivityContextInterfaceFactory;
 import org.mobicents.slee.resource.sip.SipResourceAdaptorSbbInterface;
 
 /**
- *
+ * 
  * @author Oleg Kulikov
  */
 public abstract class CallSbb implements Sbb {
 
-    private static int CALL_ID_GEN = 1;
-    private static int GEN = 1;
-    
-    public final static String ENDPOINT_NAME = "media/test/Loopback/1";
-    public final static String HELLO_WORLD = "http://localhost/mgcpdemo/audio/helloworld.wav";
-    
-    private SbbContext sbbContext;
-    
-    //SIP
-    private SipResourceAdaptorSbbInterface fp;
-    private SipProvider sipProvider;
-    private AddressFactory addressFactory;
-    private HeaderFactory headerFactory;
-    private MessageFactory messageFactory;
-    private SipActivityContextInterfaceFactory acif;
-    
-    //MGCP
-    private JainMgcpProvider mgcpProvider;
-    private MgcpActivityContextInterfaceFactory mgcpAcif;
-    
-    
-    private Logger logger = Logger.getLogger(CallSbb.class);
+	private static int CALL_ID_GEN = 1;
+	private static int GEN = 1;
 
-    /** Creates a new instance of CallSbb */
-    public CallSbb() {
-    }
+	public final static String ENDPOINT_NAME = "media/test/Loopback/1";
+	public final static String HELLO_WORLD = "http://localhost/mgcpdemo/audio/helloworld.wav";
 
-    public void onCallCreated(RequestEvent evt, ActivityContextInterface aci) {
-        Request request = evt.getRequest();
-        respond(evt, Response.TRYING);
+	private SbbContext sbbContext;
 
-        FromHeader from = (FromHeader) request.getHeader(FromHeader.NAME);
-        ToHeader to = (ToHeader) request.getHeader(ToHeader.NAME);
+	// SIP
+	private SipResourceAdaptorSbbInterface fp;
+	private SipProvider sipProvider;
+	private AddressFactory addressFactory;
+	private HeaderFactory headerFactory;
+	private MessageFactory messageFactory;
+	private SipActivityContextInterfaceFactory acif;
 
-        logger.info("Incoming call " + from + " " + to);
+	// MGCP
+	private JainMgcpProvider mgcpProvider;
+	private MgcpActivityContextInterfaceFactory mgcpAcif;
 
-        //create Dialog and attach SBB to the Dialog Activity
-        ActivityContextInterface daci = null;
-        try {
-            Dialog dialog = sipProvider.getNewDialog(evt.getServerTransaction());
-            dialog.terminateOnBye(true);
-            daci = acif.getActivityContextInterface(dialog);
-            daci.attach(sbbContext.getSbbLocalObject());
-        } catch (Exception e) {
-            logger.error("Error during dialog creation", e);
-            respond(evt, Response.SERVER_INTERNAL_ERROR);
-            return;
-        }
+	private Logger logger = Logger.getLogger(CallSbb.class);
 
-        //respond(evt, Response.RINGING);
+	/** Creates a new instance of CallSbb */
+	public CallSbb() {
+	}
 
-        CallIdentifier callID = new CallIdentifier(Integer.toHexString(CALL_ID_GEN++));
-        EndpointIdentifier endpointID = new EndpointIdentifier(ENDPOINT_NAME, "localhost:2727");
+	public void onCallCreated(RequestEvent evt, ActivityContextInterface aci) {
+		Request request = evt.getRequest();
+		respond(evt, Response.TRYING);
 
-        CreateConnection createConnection = new CreateConnection(this,
-                callID, endpointID, ConnectionMode.SendRecv);
+		FromHeader from = (FromHeader) request.getHeader(FromHeader.NAME);
+		ToHeader to = (ToHeader) request.getHeader(ToHeader.NAME);
 
-        try {
-            String sdp = new String(evt.getRequest().getRawContent());
-            createConnection.setRemoteConnectionDescriptor(new ConnectionDescriptor(sdp));
-        } catch (ConflictingParameterException e) {
-        //should never happen
-        }
+		logger.info("Incoming call " + from + " " + to);
 
-        int txID = GEN++;
-        createConnection.setTransactionHandle(txID);
+		// create Dialog and attach SBB to the Dialog Activity
+		ActivityContextInterface daci = null;
+		try {
+			Dialog dialog = sipProvider
+					.getNewDialog(evt.getServerTransaction());
+			dialog.terminateOnBye(true);
+			daci = acif.getActivityContextInterface(dialog);
+			daci.attach(sbbContext.getSbbLocalObject());
+		} catch (Exception e) {
+			logger.error("Error during dialog creation", e);
+			respond(evt, Response.SERVER_INTERNAL_ERROR);
+			return;
+		}
 
-        MgcpEndpointActivity endpointActivity = null;
-        try {
-            endpointActivity = mgcpProvider.getEndpointActivity(endpointID);
-            ActivityContextInterface epnAci = mgcpAcif.getActivityContextInterface(endpointActivity);
-            epnAci.attach(sbbContext.getSbbLocalObject());
-        } catch (FactoryException ex) {
-            ex.printStackTrace();
-        } catch (NullPointerException ex) {
-            ex.printStackTrace();
-        } catch (UnrecognizedActivityException ex) {
-            ex.printStackTrace();
-        }
+		// respond(evt, Response.RINGING);
 
-        mgcpProvider.sendMgcpEvents(new JainMgcpEvent[]{createConnection});
-    }
+		CallIdentifier callID = new CallIdentifier(Integer
+				.toHexString(CALL_ID_GEN++));
+		EndpointIdentifier endpointID = new EndpointIdentifier(ENDPOINT_NAME,
+				"localhost:2727");
 
-    public void onCreateConnectionResponse(CreateConnectionResponse event, ActivityContextInterface aci) throws ParseException {
-        logger.info("Receive CRCX response: " + event.getTransactionHandle());
+		CreateConnection createConnection = new CreateConnection(this, callID,
+				endpointID, ConnectionMode.SendRecv);
 
-        ServerTransaction txn = getServerTransaction();
-        Request request = txn.getRequest();
-        
-        ReturnCode status = event.getReturnCode();
+		try {
+			String sdp = new String(evt.getRequest().getRawContent());
+			createConnection
+					.setRemoteConnectionDescriptor(new ConnectionDescriptor(sdp));
+		} catch (ConflictingParameterException e) {
+			// should never happen
+		}
 
-        switch (status.getValue()) {
-            case ReturnCode.TRANSACTION_EXECUTED_NORMALLY:
-                String sdp = event.getLocalConnectionDescriptor().toString();
-                
-                ContentTypeHeader contentType = null;
-                try {
-                    contentType = headerFactory.createContentTypeHeader("application", "sdp");
-                } catch (ParseException ex) {
-                }
+		int txID = GEN++;
+		createConnection.setTransactionHandle(txID);
 
-                String localAddress = sipProvider.getListeningPoints()[0].getIPAddress();
-                int localPort = sipProvider.getListeningPoints()[0].getPort();
+		MgcpConnectionActivity connectionActivity = null;
+		try {
+			connectionActivity = mgcpProvider.getConnectionActivity(txID);
+			ActivityContextInterface epnAci = mgcpAcif
+					.getActivityContextInterface(connectionActivity);
+			epnAci.attach(sbbContext.getSbbLocalObject());
+		} catch (FactoryException ex) {
+			ex.printStackTrace();
+		} catch (NullPointerException ex) {
+			ex.printStackTrace();
+		} catch (UnrecognizedActivityException ex) {
+			ex.printStackTrace();
+		}
 
-                Address contactAddress = null;
-                try {
-                    contactAddress = addressFactory.createAddress("sip:" + localAddress + ":" + localPort);
-                } catch (ParseException ex) {
-                }
-                ContactHeader contact = headerFactory.createContactHeader(contactAddress);
+		mgcpProvider.sendMgcpEvents(new JainMgcpEvent[] { createConnection });
+	}
 
-                Response response = null;
-                try {
-                    response = messageFactory.createResponse(Response.RINGING, request, contentType, sdp.getBytes());
-                } catch (ParseException ex) {
-                }
+	public void onCreateConnectionResponse(CreateConnectionResponse event,
+			ActivityContextInterface aci) throws ParseException {
+		logger.info("Receive CRCX response: " + event.getTransactionHandle());
 
-                response.setHeader(contact);
-                try {
-                    txn.sendResponse(response);
-                } catch (InvalidArgumentException ex) {
-                } catch (SipException ex) {
-                }
-                break;
-            default:
-                try {
-                    response = messageFactory.createResponse(Response.SERVER_INTERNAL_ERROR, request);
-                    txn.sendResponse(response);
-                } catch (Exception ex) {
-                }
-        }
-    }
+		ServerTransaction txn = getServerTransaction();
+		Request request = txn.getRequest();
 
+		ReturnCode status = event.getReturnCode();
 
-    public void onCallTerminated(RequestEvent evt, ActivityContextInterface aci) {
-        EndpointIdentifier endpointID = new EndpointIdentifier("localhost", ENDPOINT_NAME);
-        DeleteConnection deleteConnection = new DeleteConnection(this, endpointID);
+		switch (status.getValue()) {
+		case ReturnCode.TRANSACTION_EXECUTED_NORMALLY:
 
-        int txID = GEN++;
+			this.setConnectionIdentifier(event.getConnectionIdentifier()
+					.toString());
+			String sdp = event.getLocalConnectionDescriptor().toString();
 
-        deleteConnection.setTransactionHandle(txID);
-        mgcpProvider.sendMgcpEvents(new JainMgcpEvent[]{deleteConnection});
-    }
+			ContentTypeHeader contentType = null;
+			try {
+				contentType = headerFactory.createContentTypeHeader(
+						"application", "sdp");
+			} catch (ParseException ex) {
+			}
 
-    private void respond(RequestEvent evt, int cause) {
-        Request request = evt.getRequest();
-        ServerTransaction tx = evt.getServerTransaction();
-        try {
-            Response response = messageFactory.createResponse(cause, request);
-            tx.sendResponse(response);
-        } catch (Exception e) {
-            logger.warn("Unexpected error: ", e);
-        }
-    }
+			String localAddress = sipProvider.getListeningPoints()[0]
+					.getIPAddress();
+			int localPort = sipProvider.getListeningPoints()[0].getPort();
 
-    private ServerTransaction getServerTransaction() {
-        ActivityContextInterface[] activities = sbbContext.getActivities();
-        for (ActivityContextInterface activity : activities) {
-            if (activity.getActivity() instanceof ServerTransaction) {
-                return (ServerTransaction) activity.getActivity();
-            }
-        }
-        return null;
-    }
+			Address contactAddress = null;
+			try {
+				contactAddress = addressFactory.createAddress("sip:"
+						+ localAddress + ":" + localPort);
+			} catch (ParseException ex) {
+			}
+			ContactHeader contact = headerFactory
+					.createContactHeader(contactAddress);
 
-    private Dialog getDialog() {
-        ActivityContextInterface[] activities = sbbContext.getActivities();
-        for (ActivityContextInterface activity : activities) {
-            if (activity.getActivity() instanceof Dialog) {
-                return (Dialog) activity.getActivity();
-            }
-        }
-        return null;
-    }
+			Response response = null;
+			try {
+				response = messageFactory.createResponse(Response.RINGING,
+						request, contentType, sdp.getBytes());
+			} catch (ParseException ex) {
+			}
 
-    public void setSbbContext(SbbContext sbbContext) {
-        this.sbbContext = sbbContext;
-        try {
-            Context ctx = (Context) new InitialContext().lookup("java:comp/env");
+			response.setHeader(contact);
+			try {
+				txn.sendResponse(response);
+			} catch (InvalidArgumentException ex) {
+			} catch (SipException ex) {
+			}
+			break;
+		default:
+			try {
+				response = messageFactory.createResponse(
+						Response.SERVER_INTERNAL_ERROR, request);
+				txn.sendResponse(response);
+			} catch (Exception ex) {
+			}
+		}
+	}
 
-            //initialize SIP API
-            fp = (SipResourceAdaptorSbbInterface) ctx.lookup("slee/resources/jainsip/1.2/provider");
-            sipProvider = fp.getSipProvider();
-            addressFactory = fp.getAddressFactory();
-            headerFactory = fp.getHeaderFactory();
-            messageFactory = fp.getMessageFactory();
-            acif = (SipActivityContextInterfaceFactory) ctx.lookup("slee/resources/jainsip/1.2/acifactory");
+	public void onCallTerminated(RequestEvent evt, ActivityContextInterface aci) {
+		EndpointIdentifier endpointID = new EndpointIdentifier(ENDPOINT_NAME,
+				"localhost:2727");
+		DeleteConnection deleteConnection = new DeleteConnection(this,
+				endpointID);
 
-            //initialize media api
+		deleteConnection.setConnectionIdentifier(new ConnectionIdentifier(this
+				.getConnectionIdentifier()));
 
-            mgcpProvider = (JainMgcpProvider) ctx.lookup("slee/resources/jainmgcp/2.0/provider/demo");
-            mgcpAcif = (MgcpActivityContextInterfaceFactory) ctx.lookup("slee/resources/jainmgcp/2.0/acifactory/demo");
+		int txID = GEN++;
 
-        } catch (Exception ne) {
-            logger.error("Could not set SBB context:", ne);
-        }
-    }
+		deleteConnection.setTransactionHandle(txID);
+		mgcpProvider.sendMgcpEvents(new JainMgcpEvent[] { deleteConnection });
+	}
 
-    public void unsetSbbContext() {
-    }
+	private void respond(RequestEvent evt, int cause) {
+		Request request = evt.getRequest();
+		ServerTransaction tx = evt.getServerTransaction();
+		try {
+			Response response = messageFactory.createResponse(cause, request);
+			tx.sendResponse(response);
+		} catch (Exception e) {
+			logger.warn("Unexpected error: ", e);
+		}
+	}
 
-    public void sbbCreate() throws CreateException {
-    }
+	private ServerTransaction getServerTransaction() {
+		ActivityContextInterface[] activities = sbbContext.getActivities();
+		for (ActivityContextInterface activity : activities) {
+			if (activity.getActivity() instanceof ServerTransaction) {
+				return (ServerTransaction) activity.getActivity();
+			}
+		}
+		return null;
+	}
 
-    public void sbbPostCreate() throws CreateException {
-    }
+	private Dialog getDialog() {
+		ActivityContextInterface[] activities = sbbContext.getActivities();
+		for (ActivityContextInterface activity : activities) {
+			if (activity.getActivity() instanceof Dialog) {
+				return (Dialog) activity.getActivity();
+			}
+		}
+		return null;
+	}
 
-    public void sbbActivate() {
-    }
+	public void setSbbContext(SbbContext sbbContext) {
+		this.sbbContext = sbbContext;
+		try {
+			Context ctx = (Context) new InitialContext()
+					.lookup("java:comp/env");
 
-    public void sbbPassivate() {
-    }
+			// initialize SIP API
+			fp = (SipResourceAdaptorSbbInterface) ctx
+					.lookup("slee/resources/jainsip/1.2/provider");
+			sipProvider = fp.getSipProvider();
+			addressFactory = fp.getAddressFactory();
+			headerFactory = fp.getHeaderFactory();
+			messageFactory = fp.getMessageFactory();
+			acif = (SipActivityContextInterfaceFactory) ctx
+					.lookup("slee/resources/jainsip/1.2/acifactory");
 
-    public void sbbLoad() {
-    }
+			// initialize media api
 
-    public void sbbStore() {
-    }
+			mgcpProvider = (JainMgcpProvider) ctx
+					.lookup("slee/resources/jainmgcp/2.0/provider");
+			mgcpAcif = (MgcpActivityContextInterfaceFactory) ctx
+					.lookup("slee/resources/jainmgcp/2.0/acifactory");
 
-    public void sbbRemove() {
-    }
+		} catch (Exception ne) {
+			logger.error("Could not set SBB context:", ne);
+		}
+	}
 
-    public void sbbExceptionThrown(Exception exception, Object object, ActivityContextInterface activityContextInterface) {
-    }
+	public abstract String getConnectionIdentifier();
 
-    public void sbbRolledBack(RolledBackContext rolledBackContext) {
-    }
+	public abstract void setConnectionIdentifier(String connectionIdentifier);
+
+	public void unsetSbbContext() {
+	}
+
+	public void sbbCreate() throws CreateException {
+	}
+
+	public void sbbPostCreate() throws CreateException {
+	}
+
+	public void sbbActivate() {
+	}
+
+	public void sbbPassivate() {
+	}
+
+	public void sbbLoad() {
+	}
+
+	public void sbbStore() {
+	}
+
+	public void sbbRemove() {
+	}
+
+	public void sbbExceptionThrown(Exception exception, Object object,
+			ActivityContextInterface activityContextInterface) {
+	}
+
+	public void sbbRolledBack(RolledBackContext rolledBackContext) {
+	}
 }
