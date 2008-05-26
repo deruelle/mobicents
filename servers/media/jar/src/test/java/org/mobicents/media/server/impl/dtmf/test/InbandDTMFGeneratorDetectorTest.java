acafab3cd1ee3ec581dcc0b2444e9c31686028ec
baranowb <baranowb@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2008-05-26 16:51:57 +0000

    initial impl of inband dtmf tests.
    git-svn-id: http://mobicents.googlecode.com/svn/trunk@1762 bf0df8d0-2c1f-0410-b170-bd30377b63dc

---
+++ servers/media/jar/src/test/java/org/mobicents/media/server/impl/dtmf/test/InbandDTMFGeneratorDetectorTest.java
@@ -0,0 +1,79 @@
+package org.mobicents.media.server.impl.dtmf.test;
+
+import java.util.Arrays;
+
+import org.apache.log4j.Logger;
+import org.mobicents.media.server.impl.NewSuperXCase;
+import org.mobicents.media.server.impl.dtmf.InbandDetector;
+import org.mobicents.media.server.impl.dtmf.InbandGenerator;
+import org.mobicents.media.server.impl.dtmf.test.InbandDTMFGeneratorSequentialTest.FrequenciesHolder;
+import org.mobicents.media.server.impl.fft.SpectralAnalyser;
+import org.mobicents.media.server.spi.NotificationListener;
+import org.mobicents.media.server.spi.events.NotifyEvent;
+import org.mobicents.media.server.spi.events.test.SpectrumEvent;
+
+public class InbandDTMFGeneratorDetectorTest extends
+		InbandDTMFGeneratorSequentialTest {
+
+	protected boolean timeCaptureTest = false;
+
+	public InbandDTMFGeneratorDetectorTest() {
+		super();
+		// 70ms
+		super.TEST_DURATION = 70;
+	}
+
+	protected void setUp(int row, int column) throws Exception {
+
+		String toneName = InbandGenerator.getToneName(row, column);
+		if (toneName != null) {
+			gen = new InbandGenerator(toneName, TEST_DURATION);
+
+			analyzer = new InbandDetector();
+
+		} else {
+			throw new RuntimeException("Failed to obtain tone name for [" + row
+					+ "][" + column + "]");
+		}
+
+	}
+
+	public void testChangingCaptureTime() throws Exception {
+		// Here we will test until failure to see how far we can go
+		try {
+			timeCaptureTest = true;
+			super.TEST_DURATION = 100;
+
+			for (; super.TEST_DURATION > 50; super.TEST_DURATION--) {
+				//testDigit6();
+			}
+		} finally {
+			timeCaptureTest = false;
+		}
+
+	}
+
+	@Override
+	public void update(NotifyEvent event) {
+
+		super.notified = true;
+		String toneName = InbandGenerator.getToneName(super.currentRow,
+				super.currentColumn);
+		if (event.getMessage().compareToIgnoreCase(toneName) != 0
+				&& !timeCaptureTest) {
+			doFail("Tone name[" + toneName
+					+ "] didnt match detected tone name[" + event.getMessage()
+					+ "]. Old reason[" + getReason() + "]");
+		} else if (timeCaptureTest) {
+			logger.info("Failed to capture properly tone[" + toneName
+					+ "] - captured value indicates[" + event.getMessage()
+					+ "], this happened for capture time["
+					+ super.TEST_DURATION
+					+ "]. This is only informational procedure.");
+		} else {
+
+		}
+
+	}
+
+}
