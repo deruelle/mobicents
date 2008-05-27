ac2d4e74abaeee69db2b8812db3c2702cc05ca83
baranowb <baranowb@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2008-05-27 07:23:02 +0000

    Constants for Junit tests resources.
    git-svn-id: http://mobicents.googlecode.com/svn/trunk@1769 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- servers/media/constants/src/main/java/org/mobicents/media/server/impl/common/MediaResourceType.java
+++ servers/media/constants/src/main/java/org/mobicents/media/server/impl/common/MediaResourceType.java
@@ -3,7 +3,7 @@ package org.mobicents.media.server.impl.common;
 public enum MediaResourceType {
 
     AUDIO_SOURCE("AUDIO_SOURCE"), AUDIO_SINK("AUDIO_SINK"),
-    DTMF_DETECTOR("DTMF_DETECTOR"), DTMF_GENERATOR("DTMF_GENERATOR");
+    DTMF_DETECTOR("DTMF_DETECTOR"), DTMF_GENERATOR("DTMF_GENERATOR"),SPECTRUM_ANALYSER("SPECTRUM_ANALYSER");
 
     private MediaResourceType(String name) {
         this.name = name;
--- servers/media/constants/src/main/java/org/mobicents/media/server/impl/common/events/EventID.java
+++ servers/media/constants/src/main/java/org/mobicents/media/server/impl/common/events/EventID.java
@@ -3,7 +3,7 @@ package org.mobicents.media.server.impl.common.events;
 import java.io.StreamCorruptedException;
 
 public enum EventID {
-	 PLAY, COMPLETE,FAIL,PLAY_RECORD,PROMPT_AND_COLLECT,DTMF;
+	 PLAY, COMPLETE,FAIL,PLAY_RECORD,PROMPT_AND_COLLECT,DTMF,TEST_SPECTRA;
 
 	
 }
