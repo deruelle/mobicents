a403d806cd578ab575ba61686ac264e25c5f0728
oleg.kulikoff <oleg.kulikoff@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2008-05-27 15:22:08 +0000

    Resolve confilcts
    git-svn-id: http://mobicents.googlecode.com/svn/trunk@1786 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- servers/media/constants/src/main/java/org/mobicents/media/server/impl/common/MediaResourceType.java
+++ servers/media/constants/src/main/java/org/mobicents/media/server/impl/common/MediaResourceType.java
@@ -2,8 +2,11 @@ package org.mobicents.media.server.impl.common;
 
 public enum MediaResourceType {
 
-    AUDIO_SOURCE("AUDIO_SOURCE"), AUDIO_SINK("AUDIO_SINK"),
-    DTMF_DETECTOR("DTMF_DETECTOR"), DTMF_GENERATOR("DTMF_GENERATOR"),SPECTRUM_ANALYSER("SPECTRUM_ANALYSER");
+    AUDIO_SOURCE("AUDIO_SOURCE"), 
+    AUDIO_SINK("AUDIO_SINK"),
+    DTMF_DETECTOR("DTMF_DETECTOR"), 
+    DTMF_GENERATOR("DTMF_GENERATOR"),
+    SPECTRUM_ANALYSER("SPECTRUM_ANALYZER");
 
     private MediaResourceType(String name) {
         this.name = name;
--- servers/media/constants/src/main/java/org/mobicents/media/server/impl/common/events/EventID.java
+++ servers/media/constants/src/main/java/org/mobicents/media/server/impl/common/events/EventID.java
@@ -3,7 +3,8 @@ package org.mobicents.media.server.impl.common.events;
 import java.io.StreamCorruptedException;
 
 public enum EventID {
-	 PLAY, COMPLETE,FAIL,PLAY_RECORD,PROMPT_AND_COLLECT,DTMF,TEST_SPECTRA;
+	 PLAY, COMPLETE,FAIL,PLAY_RECORD,PROMPT_AND_COLLECT,DTMF,
+         TEST_SPECTRA;
 
 	
 }
