937fe944ebc754852befb7c41d51c1fc566b3536
amit.bhayani@gmail.com <amit.bhayani@gmail.com@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2008-06-09 13:37:35 +0000

    Added MsConnectionState
    git-svn-id: http://mobicents.googlecode.com/svn/trunk@1935 bf0df8d0-2c1f-0410-b170-bd30377b63dc

---
+++ servers/media/constants/src/main/java/org/mobicents/media/msc/common/MsConnectionState.java
@@ -0,0 +1,25 @@
+package org.mobicents.media.msc.common;
+
+/**
+ * <code>MsConnectionState</code> represents the state of MsConnection.
+ * <ul>
+ * <li>IDLE : When the MsConnection is created for first time state is IDLE</li>
+ * <li>HALF_OPEN : When the MsConnection.modify() is called but without remote
+ * SDP and creationg of Connection is successful the state is HALF_OPEN</li>
+ * 
+ * <li>OPEN : When the MsConnection.modify() is called with remote SDP and the
+ * Connection is successfully modified the state is OPEN</li>
+ * 
+ * <li>FAILED : When the creation or modification of MsConnection fails for any
+ * reason the state is FAILED</li>
+ * 
+ * <li>CLOSED : When the MsConnection.release() is called state is CLOSED</li>
+ * 
+ * </ul>
+ * 
+ * @author amit bhayani
+ * 
+ */
+public enum MsConnectionState {
+	IDLE, HALF_OPEN, OPEN, FAILED, CLOSED
+}
