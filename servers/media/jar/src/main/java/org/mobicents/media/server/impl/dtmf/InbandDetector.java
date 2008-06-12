dfdf44088265dab64ed7d8d6d5c061fcc7b43ba8
oleg.kulikoff <oleg.kulikoff@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2008-06-12 15:00:11 +0000

    Apply Hamming window for DTMF detection
    git-svn-id: http://mobicents.googlecode.com/svn/trunk@1960 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- servers/media/jar/src/main/java/org/mobicents/media/server/impl/dtmf/InbandDetector.java
+++ servers/media/jar/src/main/java/org/mobicents/media/server/impl/dtmf/InbandDetector.java
@@ -23,7 +23,6 @@ import org.mobicents.media.format.UnsupportedFormatException;
 import org.mobicents.media.goertzel.Filter;
 import org.mobicents.media.protocol.BufferTransferHandler;
 import org.mobicents.media.protocol.PushBufferStream;
-import org.mobicents.media.server.impl.BaseEndpoint;
 import org.mobicents.media.server.impl.common.MediaResourceState;
 import org.mobicents.media.server.impl.jmf.dsp.Codec;
 import org.mobicents.media.server.impl.jmf.dsp.CodecLocator;
@@ -31,59 +30,109 @@ import org.mobicents.media.server.impl.common.MediaResourceState;
 import org.mobicents.media.server.spi.Endpoint;
 
 /**
+ * Implements inband DTMF detector.
  * 
+ * Inband means that DTMF is transmitted within the audio of the phone 
+ * conversation, i.e. it is audible to the conversation partners. Therefore only 
+ * uncompressed codecs like g711 alaw or ulaw can carry inband DTMF reliably. 
+ * Female voice are known to once in a while trigger the recognition of a DTMF 
+ * tone. For analog lines inband is the only possible means to transmit DTMF. 
+ *
  * @author Oleg Kulikov
  */
-public class InbandDetector extends BaseDtmfDetector implements
-        BufferTransferHandler {
+public class InbandDetector extends BaseDtmfDetector implements BufferTransferHandler {
 
     public final static String[][] events = new String[][]{
-        {"1", "2", "3", "A"}, {"4", "5", "6", "B"},
-        {"7", "8", "9", "C"}, {"*", "0", "#", "D"}};
+        {"1", "2", "3", "A"}, 
+        {"4", "5", "6", "B"},
+        {"7", "8", "9", "C"}, 
+        {"*", "0", "#", "D"}
+    };
+    
+    /** DTMF tone duration in milliseconds */
+    private int TONE_DURATION = 50;
+    private double THRESHOLD = 30;
     
     private int[] lowFreq = new int[]{697, 770, 852, 941};
     private int[] highFreq = new int[]{1209, 1336, 1477, 1633};
+    
     private Codec codec;
-    private byte[] localBuffer = new byte[16000];
+    private byte[] localBuffer;
     private int offset = 0;
-    private Filter filter = new Filter(10);
-    private double threshold = 10;
+    private Filter filter = null;
     
     private Logger logger = Logger.getLogger(InbandDetector.class);
 
+    /**
+     * Creates new instance of Detector.
+     */
     public InbandDetector() {
         super();
-        try {
-            filter = new Filter();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        filter = new Filter();
     }
 
+    /**
+     * (Non Java-doc).
+     * 
+     * @see org.mobicents.media.server.spi.MediaSink#Configure(Properties).
+     */
     public void configure(Properties config) {
+        if (config != null) {
+            //read duration if present
+            try {
+                TONE_DURATION = Integer.parseInt(config.getProperty("tone.duration"));
+            } catch (Exception e) {
+            }
+            //read threshold if present
+            try {
+                THRESHOLD = 10 * Double.parseDouble(config.getProperty("detector.theshold"));
+            } catch (Exception e) {
+            }
+        }
+        
+        localBuffer = new byte[16 * TONE_DURATION];
         setState(MediaResourceState.CONFIGURED);
     }
 
+    /**
+     * (Non Java-doc).
+     * 
+     * @see org.mobicents.media.server.spi.MediaSink#prepare(Endpoint, PushBufferStream).
+     */
     public void prepare(Endpoint endpoint, PushBufferStream stream) throws UnsupportedFormatException {
         stream.setTransferHandler(this);
         if (!stream.getFormat().matches(Codec.LINEAR_AUDIO)) {
-            codec = CodecLocator.getCodec(stream.getFormat(),
-                    Codec.LINEAR_AUDIO);
-            setState(MediaResourceState.PREPARED);
+            codec = CodecLocator.getCodec(stream.getFormat(),Codec.LINEAR_AUDIO);
             if (codec == null) {
                 throw new UnsupportedFormatException(stream.getFormat());
             }
+            setState(MediaResourceState.PREPARED);
         }
     }
 
+    /**
+     * (Non Java-doc).
+     * 
+     * @see org.mobicents.media.server.spi.MediaSink#start().
+     */
     public void start() {
         setState(MediaResourceState.STARTED);
     }
 
+    /**
+     * (Non Java-doc).
+     * 
+     * @see org.mobicents.media.server.spi.MediaSink#stop().
+     */
     public void stop() {
         setState(MediaResourceState.PREPARED);
     }
 
+    /**
+     * (Non Java-doc).
+     * 
+     * @see org.mobicents.media.protocol.BufferTransferHandler.transferData().
+     */
     public void transferData(PushBufferStream stream) {
         if (getState() != MediaResourceState.STARTED) {
             return;
@@ -101,45 +150,32 @@ public class InbandDetector extends BaseDtmfDetector implements
             data = codec.process(data);
         }
 
-        int len = Math.min(8000/*16000*/ - offset, data.length);
+        int len = Math.min(localBuffer.length - offset, data.length);
         System.arraycopy(data, 0, localBuffer, offset, len);
         offset += len;
 
-        if (logger.isDebugEnabled()) {
-            logger.debug("append " + len + " bytes to the local buffer, buff size=" + offset);
-        }
-
         // buffer full?
-        if (offset == 8000/*16000*/) {
-            double[] signal = new double[4000/*8000*/];
+        if (offset == localBuffer.length) {
+            double[] signal = new double[localBuffer.length / 2];
             int k = 0;
             for (int i = 0; i < signal.length; i++) {
                 signal[i] = ((localBuffer[k++] & 0xff) | (localBuffer[k++] << 8));
             }
 
-            localBuffer = new byte[8000/*16000*/];
+            localBuffer = new byte[localBuffer.length];
             offset = 0;
 
             double p[] = getPower(lowFreq, signal);
-//            System.out.println("LOW----------------------");
-//            print(p);
-            
-//            System.out.println("HIGH----------------------");
             double P[] = getPower(highFreq, signal);
-//            print(P);
+            
             String tone = getTone(p, P);
+            
             if (tone != null) {
                 digitBuffer.push(tone);
             }
         }
     }
 
-    private void print(double p[]) {
-        for (int i = 0; i < p.length; i++) {
-            System.out.print(p[i] + " ");
-        }
-        System.out.println();
-    }
     /**
      * Calculate power of the specified frequencies.
      * 
@@ -150,7 +186,7 @@ public class InbandDetector extends BaseDtmfDetector implements
     private double[] getPower(int[] freq, double[] data) {
         double[] power = new double[freq.length];
         for (int i = 0; i < freq.length; i++) {
-            power[i] = filter.getPower(freq[i], data);
+            power[i] = filter.getPower(freq[i], data, (double)TONE_DURATION/(double)1000);
         }
         return power;
     }
@@ -172,66 +208,69 @@ public class InbandDetector extends BaseDtmfDetector implements
         }
         return idx;
     }
-    
+
+    /**
+     * Searches DTMF tone.
+     * 
+     * @param f the low frequency array
+     * @param F the high frequency array.
+     * @return DTMF tone.
+     */
     private String getTone(double f[], double F[]) {
         int fm = getMax(f);
-//        System.out.println("fm=" + fm);
         boolean fd = true;
-        
-        double ft = threshold;
+
         for (int i = 0; i < f.length; i++) {
-            if (fm == i) continue;
-            double r = f[fm]/(f[i] + 1E-15);
-//            System.out.println("r=" + r);
-            if (r < threshold) {
+            if (fm == i) {
+                continue;
+            }
+            double r = f[fm] / (f[i] + 1E-15);
+            if (r < THRESHOLD) {
                 fd = false;
                 break;
-            } else if (r > ft) {
-                ft = r;
-            }
+            } 
         }
-        
-        ft = threshold + (ft - threshold)/2;
-//        System.out.println("++++ ft=" + ft);
-        
-        if (!fd) return null;
+
+        if (!fd) {
+            return null;
+        }
 
         int Fm = getMax(F);
-//        System.out.println("Fm=" + Fm);
         boolean Fd = true;
-        
+
         for (int i = 0; i < F.length; i++) {
-            if (Fm == i) continue;
-            double r = F[Fm]/(F[i] + 1E-15);
-//            System.out.println("R=" + r);
-            if (r < threshold) {
+            if (Fm == i) {
+                continue;
+            }
+            double r = F[Fm] / (F[i] + 1E-15);
+            if (r < THRESHOLD) {
                 Fd = false;
                 break;
             }
         }
-        
-        if (!Fd) return null;
-        
+
+        if (!Fd) {
+            return null;
+        }
+
         return events[fm][Fm];
     }
-    
-    private int checkFreq(int[] freq, double[] data) {
-        for (int i = 0; i < freq.length; i++) {
-            if (logger.isDebugEnabled()) {
-                logger.debug("Checking freq: " + freq[i]);
-            }
-            if (filter.detect(freq[i], data)) {
-                return i;
-            }
-        }
-        return -1;
-    }
 
+    /**
+     * (Non Java-doc).
+     * 
+     * @see org.mobicents.media.server.spi.MediaResource#release().
+     */
     public void release() {
         localBuffer = null;
         setState(MediaResourceState.NULL);
     }
 
+    /**
+     * (Non Java-doc).
+     * 
+     * @see org.mobicents.media.server.spi.MediaSink#newBranch(String).
+     */
     public PushBufferStream newBranch(String branchID) {
         throw new UnsupportedOperationException("Not supported.");
     }
