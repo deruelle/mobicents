15dd72086a03ea4719419ce67c4ec564de62a900
oleg.kulikoff <oleg.kulikoff@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2008-06-24 04:19:01 +0000

    Added event identifier: TEST_SINE
    git-svn-id: http://mobicents.googlecode.com/svn/trunk@2032 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- servers/media/constants/src/main/java/org/mobicents/media/server/impl/common/events/EventID.java
+++ servers/media/constants/src/main/java/org/mobicents/media/server/impl/common/events/EventID.java
@@ -1,10 +1,8 @@
 package org.mobicents.media.server.impl.common.events;
 
-import java.io.StreamCorruptedException;
-
 public enum EventID {
 	 PLAY, COMPLETE,FAIL,PLAY_RECORD,PROMPT_AND_COLLECT,DTMF,
-         TEST_SPECTRA;
+         TEST_SPECTRA, TEST_SINE;
 
 	
 }
