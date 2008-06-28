6a06805ca7cbd20565243904b956d4710e3af541
amit.bhayani@gmail.com <amit.bhayani@gmail.com@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2008-06-28 11:37:18 +0000

    removed everything from servers/media
    git-svn-id: http://mobicents.googlecode.com/svn/trunk@2115 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- servers/media/constants/src/main/java/org/mobicents/media/server/impl/common/events/EventID.java
+++
@@ -1,8 +0,0 @@
-package org.mobicents.media.server.impl.common.events;
-
-public enum EventID {
-	 PLAY, COMPLETE,FAIL,PLAY_RECORD,PROMPT_AND_COLLECT,DTMF,
-         TEST_SPECTRA, TEST_SINE;
-
-	
-}
--- servers/media/jar/src/main/java/org/mobicents/media/server/impl/fft/DFTEndpointManagementMBean.java
+++
@@ -1,7 +0,0 @@
-package org.mobicents.media.server.impl.fft;
-
-import org.mobicents.media.server.impl.jmx.EndpointManagementMBean;
-
-public interface DFTEndpointManagementMBean extends EndpointManagementMBean {
-
-}
