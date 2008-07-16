8543c9094fcd3255c23022154349b258cbf6b56c
oleg.kulikoff <oleg.kulikoff@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2008-07-16 15:21:44 +0000

    Perform encoding/decoding
    git-svn-id: http://mobicents.googlecode.com/svn/trunk@2337 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- servers/media/core/server-impl/src/main/java/org/mobicents/media/server/impl/jmf/dsp/audio/speex/Decoder.java
+++ servers/media/core/server-impl/src/main/java/org/mobicents/media/server/impl/jmf/dsp/audio/speex/Decoder.java
@@ -6,36 +6,39 @@ import org.mobicents.media.Format;
 import org.mobicents.media.server.impl.jmf.dsp.Codec;
 import org.xiph.speex.SpeexDecoder;
 
-
 public class Decoder implements Codec {
 
-	private SpeexDecoder decoder = null;
+    private final static int MODE_NB = 0;
+    private final static boolean ENHANCED = false;
+    private final static int SAMPLE_RATE = 8000;
+    private final static int CHANNELS = 1;
+    private SpeexDecoder decoder = new SpeexDecoder();
 
-	public Format[] getSupportedInputFormats() {
-		Format[] formats = new Format[] { Codec.SPEEX };
-		return formats;
-	}
+    public Decoder() {
+        decoder.init(MODE_NB, SAMPLE_RATE, CHANNELS, ENHANCED);
+    }
 
-	public Format[] getSupportedOutputFormats(Format fmt) {
-		Format[] formats = new Format[] { Codec.LINEAR_AUDIO };
-		return formats;
-	}
+    public Format[] getSupportedInputFormats() {
+        Format[] formats = new Format[]{Codec.SPEEX};
+        return formats;
+    }
 
-	public byte[] process(byte[] media) {
-		byte[] output = null;
-		decoder = new SpeexDecoder();
+    public Format[] getSupportedOutputFormats(Format fmt) {
+        Format[] formats = new Format[]{Codec.LINEAR_AUDIO};
+        return formats;
+    }
 
-		decoder.init(0, 8000, 1, false);
-		
-		try {
-			decoder.processData(media, 0, media.length);
-			output = new byte[decoder.getProcessedDataByteSize()];
-			decoder.getProcessedData(output, 0);
-		} catch (StreamCorruptedException e) {			
-			e.printStackTrace();
-		}
-		
-		return output;
-	}
+    public byte[] process(byte[] media) {
+        byte[] output = null;
+        
+        try {
+            decoder.processData(media, 0, media.length);
+            output = new byte[decoder.getProcessedDataByteSize()];
+            decoder.getProcessedData(output, 0);
+        } catch (StreamCorruptedException e) {
+            e.printStackTrace();
+        }
 
+        return output;
+    }
 }
--- servers/media/core/server-impl/src/main/java/org/mobicents/media/server/impl/jmf/dsp/audio/speex/Encoder.java
+++ servers/media/core/server-impl/src/main/java/org/mobicents/media/server/impl/jmf/dsp/audio/speex/Encoder.java
@@ -6,43 +6,31 @@ import org.xiph.speex.SpeexEncoder;
 
 public class Encoder implements Codec {
 
-	private SpeexEncoder speexEncoder = null;
+    private final static int MODE_NB = 0;
+    private final static int QUALITY = 3;
+    private final static int SAMPLE_RATE = 8000;
+    private final static int CHANNELS = 1;
+    private SpeexEncoder speexEncoder = new SpeexEncoder();
 
-	public Format[] getSupportedInputFormats() {
-		Format[] formats = new Format[] { Codec.LINEAR_AUDIO };
-		return formats;
-	}
+    public Encoder() {
+        speexEncoder.init(MODE_NB, QUALITY, SAMPLE_RATE, CHANNELS);
+    }
 
-	public Format[] getSupportedOutputFormats(Format fmt) {
-		Format[] formats = new Format[] { Codec.SPEEX };
-		return formats;
-	}
+    public Format[] getSupportedInputFormats() {
+        Format[] formats = new Format[]{Codec.LINEAR_AUDIO};
+        return formats;
+    }
 
-	public byte[] process(byte[] media) {
-		speexEncoder = new SpeexEncoder();
-		speexEncoder.init(0, 3, 8000, 1);
-		
-//		Buffer input = new Buffer();		
-//		input.setFormat(getSupportedInputFormats()[0]);
-//		input.setData(media);
-//		input.setLength(media.length);
-//		
-//		
-//		Buffer output = new Buffer();
-//		
-//		 byte[] compressed = new byte[media.length / 2];
-		
-		int channels = 1;
-		
-		int pcmPacketSize = 2 * channels * speexEncoder.getFrameSize();
-		
-		speexEncoder.processData(media, 0, pcmPacketSize);
-		
-		byte[] buff = new byte[speexEncoder.getProcessedDataByteSize()];
-		
-		speexEncoder.getProcessedData(buff, 0);
-		 
-		return buff;
-	}
+    public Format[] getSupportedOutputFormats(Format fmt) {
+        Format[] formats = new Format[]{Codec.SPEEX};
+        return formats;
+    }
 
+    public byte[] process(byte[] media) {
+        speexEncoder.processData(media, 0, media.length);
+        int size = speexEncoder.getProcessedDataByteSize();
+        byte[] buff = new byte[size];
+        speexEncoder.getProcessedData(buff, 0);
+        return buff;
+    }
 }
