3e0541c3bf9ed462225bc8af8d6da4873b6cbb8f
oleg.kulikoff <oleg.kulikoff@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2008-08-06 09:53:24 +0000

    Revert to 2468
    git-svn-id: http://mobicents.googlecode.com/svn/trunk@2487 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- servers/media/controllers/mgcp/mgcp-controller-sbb/notificationrequest-sbb/src/main/java/org/mobicents/media/server/control/mgcp/NotificationRequestSbb.java
+++ servers/media/controllers/mgcp/mgcp-controller-sbb/notificationrequest-sbb/src/main/java/org/mobicents/media/server/control/mgcp/NotificationRequestSbb.java
@@ -12,10 +12,11 @@
  * but not limited to the correctness, accuracy, reliability or
  * usefulness of the software.
  */
-
 package org.mobicents.media.server.control.mgcp;
 
 import jain.protocol.ip.mgcp.message.NotificationRequest;
+import jain.protocol.ip.mgcp.message.parms.EndpointIdentifier;
+import jain.protocol.ip.mgcp.message.parms.RequestedEvent;
 import jain.protocol.ip.mgcp.message.parms.ReturnCode;
 
 import javax.naming.Context;
@@ -36,81 +37,81 @@ import org.apache.log4j.Logger;
  */
 public abstract class NotificationRequestSbb implements Sbb {
 
-	private SbbContext sbbContext;
-	private Logger logger = Logger.getLogger(NotificationRequestSbb.class);
+    private SbbContext sbbContext;
+    private Logger logger = Logger.getLogger(NotificationRequestSbb.class);
+    private JainMgcpProvider mgcpProvider;
 
-	private JainMgcpProvider mgcpProvider;
+    /**
+     * Creates a new instance of CreateConnectionSbb
+     * 
+     */
+    public NotificationRequestSbb() {
+    }
 
-	/**
-	 * Creates a new instance of CreateConnectionSbb
-	 * 
-	 */
-	public NotificationRequestSbb() {
-	}
+    public void setSbbContext(SbbContext sbbContext) {
+        this.sbbContext = sbbContext;
+        try {
+            Context ctx = (Context) new InitialContext().lookup("java:comp/env");
 
-	public void setSbbContext(SbbContext sbbContext) {
-		this.sbbContext = sbbContext;
-		try {
-			Context ctx = (Context) new InitialContext().lookup("java:comp/env");
+            mgcpProvider = (JainMgcpProvider) ctx.lookup("slee/resources/jainmgcp/2.0/provider");
 
-			mgcpProvider = (JainMgcpProvider) ctx.lookup("slee/resources/jainmgcp/2.0/provider");
+        } catch (NamingException ne) {
+            logger.warn("Could not set SBB context:" + ne.getMessage());
+        }
+    }
 
-		} catch (NamingException ne) {
-			logger.warn("Could not set SBB context:" + ne.getMessage());
-		}
-	}
+    public void onNotificationRequest(NotificationRequest event, ActivityContextInterface aci) {
+        logger.info("onNotificationRequest");
+        EndpointIdentifier endpointID = event.getEndpointIdentifier();
+        RequestedEvent[] reqEvents = event.getRequestedEvents();
+        
+        //reqEvents[0].getEventName().getConnectionIdentifier()
 
-	public void onNotificationRequest(NotificationRequest event, ActivityContextInterface aci) {
-		logger.info("onNotificationRequest");
+	
+    }
 
-
-
-	}
-
-	private void sendResponse(int txID, ReturnCode reason) {
+    private void sendResponse(int txID, ReturnCode reason) {
 //		EndpointConfigurationResponse response = new EndpointConfigurationResponse(this.getReceivedTransactionID(), reason);
 //		response.setTransactionHandle(txID);
 //		logger.info("<-- TX ID = " + txID + ": " + response.getReturnCode());
 //		mgcpProvider.sendMgcpEvents(new JainMgcpEvent[] { response });
-	}
+    }
 
-	public void unsetSbbContext() {
-	}
+    public void unsetSbbContext() {
+    }
 
-	public void sbbCreate() throws CreateException {
-	}
+    public void sbbCreate() throws CreateException {
+    }
 
-	public void sbbPostCreate() throws CreateException {
-	}
+    public void sbbPostCreate() throws CreateException {
+    }
 
-	public void sbbActivate() {
-	}
+    public void sbbActivate() {
+    }
 
-	public void sbbPassivate() {
-	}
+    public void sbbPassivate() {
+    }
 
-	public void sbbLoad() {
-	}
+    public void sbbLoad() {
+    }
 
-	public void sbbStore() {
-	}
+    public void sbbStore() {
+    }
 
-	public void sbbRemove() {
-	}
+    public void sbbRemove() {
+    }
 
-	public void sbbExceptionThrown(Exception exception, Object object, ActivityContextInterface activityContextInterface) {
-	}
+    public void sbbExceptionThrown(Exception exception, Object object, ActivityContextInterface activityContextInterface) {
+    }
 
-	public void sbbRolledBack(RolledBackContext rolledBackContext) {
-	}
+    public void sbbRolledBack(RolledBackContext rolledBackContext) {
+    }
 
-	public abstract int getTxId();
+    public abstract int getTxId();
 
-	public abstract void setTxId(int txId);
-	
-	public abstract Object getReceivedTransactionID();
+    public abstract void setTxId(int txId);
 
-	public abstract void setReceivedTransactionID(Object receivedTransactionID);	
-	
+    public abstract Object getReceivedTransactionID();
 
+    public abstract void setReceivedTransactionID(Object receivedTransactionID);
 }
