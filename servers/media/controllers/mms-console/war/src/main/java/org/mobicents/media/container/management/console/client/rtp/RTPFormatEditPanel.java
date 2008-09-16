2ca40facf77699ae8b64518a4b0dd09b63e1a906
baranowb <baranowb@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2008-09-16 12:45:59 +0000

    Adds remove operations which I forgot do introduce, good that Amit is here :)
    git-svn-id: http://mobicents.googlecode.com/svn/trunk@2890 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- servers/media/controllers/mms-console/war/src/main/java/org/mobicents/media/container/management/console/client/rtp/RTPFormatEditPanel.java
+++ servers/media/controllers/mms-console/war/src/main/java/org/mobicents/media/container/management/console/client/rtp/RTPFormatEditPanel.java
@@ -22,6 +22,8 @@ public class RTPFormatEditPanel extends Composite {
 	protected XFormat xf=null;
 	protected TextBox payloadTypeBox = new TextBox();
 	protected TextBox mediaFormatBox = new TextBox();
+	protected Button confirmButton = new Button("Confirm");
+	protected Button removeButton = new Button("Remove");
 	protected RTPMediaFormatConfigurationPage page;
 	public RTPFormatEditPanel(final RTPMediaFormatConfigurationPage page) {
 		super();
@@ -33,13 +35,15 @@ public class RTPFormatEditPanel extends Composite {
 		ListPanel optionsPanel = new ListPanel();
 		optionsPanel.setHeader(0, "Payload Type");
 		optionsPanel.setHeader(1, "Media Format");
-		optionsPanel.setHeader(3, "#");
+		optionsPanel.setHeader(3, "Confirm/Add");
+		optionsPanel.setHeader(3, "Remove");
 
 		
-		Button confirmButton = new Button("Confirm");
+		
 		optionsPanel.setCell(0, 0, payloadTypeBox);
 		optionsPanel.setCell(0, 1, mediaFormatBox);
 		optionsPanel.setCell(0, 2, confirmButton);
+		optionsPanel.setCell(0, 3, removeButton);
 
 		// SETUP
 		mediaFormatBox.setEnabled(false);
@@ -52,59 +56,82 @@ public class RTPFormatEditPanel extends Composite {
 
 			public void onClick(Widget arg0) {
 				
-				try{
-					Integer.valueOf(payloadTypeBox.getText());
-				}catch(Exception e)
-				{
-					UserInterface.getLogPanel().error("Payload Type must be interger!!!");
-					return;
-				}
-				
-				xf.setRtpMap(payloadTypeBox.getText());
-				if(index>=0)
-				{
-					targetBox.setItemText(index, xf.toString());
-				}else
-				{
-					int itemCount=targetBox.getItemCount();
-					for(int i=0;i<itemCount;i++)
-					{
-						if(targetBox.getItemText(i).trim().equals(xf.toString().trim()))
-						{
-							return;
-						}
-					}
-					targetBox.addItem(xf.toString(), xf.toString());
-				}
-				
-				
-				
-				int itemCount=targetBox.getItemCount();
-				String allFormats="";
-				
-				for(int i=0;i<itemCount;i++)
-				{
-					allFormats+=targetBox.getItemText(i);
-					
-					if(i<itemCount-1)
-						allFormats+=";";
-					
-					
-				}
-				UserInterface.getLogPanel().info("Sending: "+allFormats);
-				XFormat[] xfs=XFormat.fromString(allFormats);
-				action.perform(xfs, page);
+				sendFormatsUpdate(false);
 				
 			}});
 		
-		
+		removeButton.addClickListener(new ClickListener(){
+
+			public void onClick(Widget arg0) {
+				sendFormatsUpdate(true);
+				
+			}});
 		rootPanel.setWidget(0, 0, optionsPanel);
 		
 		
 	}
 
+	private void sendFormatsUpdate(boolean deleteCurrent)
+	{
+		try{
+			Integer.valueOf(payloadTypeBox.getText());
+		}catch(Exception e)
+		{
+			UserInterface.getLogPanel().error("Payload Type must be interger!!!");
+			return;
+		}
+		if(!deleteCurrent)
+		{
+			xf.setRtpMap(payloadTypeBox.getText());
+		if(index>=0)
+		{
+			targetBox.setItemText(index, xf.toString());
+		}else
+		{
+			int itemCount=targetBox.getItemCount();
+			for(int i=0;i<itemCount;i++)
+			{
+				if(targetBox.getItemText(i).trim().equals(xf.toString().trim()))
+				{
+					return;
+				}
+			}
+			targetBox.addItem(xf.toString(), xf.toString());
+		}
+		
+		
+		}else
+		{
+			int itemCount=targetBox.getItemCount();
+			for(int i=0;i<itemCount;i++)
+			{
+				if(targetBox.getItemText(i).trim().equals(xf.toString().trim()))
+				{
+					index=-1;
+					targetBox.removeItem(i);
+					break;
+				}
+			}
+		}
+		int itemCount=targetBox.getItemCount();
+		String allFormats="";
+		
+		for(int i=0;i<itemCount;i++)
+		{
+			allFormats+=targetBox.getItemText(i);
+			
+			if(i<itemCount-1)
+				allFormats+=";";
+			
+			
+		}
+		UserInterface.getLogPanel().info("Sending: "+allFormats);
+		XFormat[] xfs=XFormat.fromString(allFormats);
+		action.perform(xfs, page);
+	}
+	
 	public void setCreateNew(XFormat xf, String objectName, PerformActionClass action, ListBox box, int index) {
-
+		this.removeButton.setEnabled(false);
 		this.index=index;
 		this.targetBox=box;
 		this.action=action;
@@ -112,15 +139,18 @@ public class RTPFormatEditPanel extends Composite {
 		this.payloadTypeBox.setText(this.xf.getRtpMap());
 		this.mediaFormatBox.setText(this.xf.getFormat());
 		
+		
 	}
 
 	public void setEdit(XFormat xf, String objectName, PerformActionClass action, ListBox presentFormatsBox, int index) {
+		this.removeButton.setEnabled(true);
 		this.index=index;
 		this.targetBox=presentFormatsBox;
 		this.action=action;
 		this.xf=xf;
 		this.payloadTypeBox.setText(this.xf.getRtpMap());
 		this.mediaFormatBox.setText(this.xf.getFormat());
+		
 	}
 
 }
