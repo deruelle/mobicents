9e0d437ebb13f4c800259cf8de4bd9f055291b67
oleg.kulikoff <oleg.kulikoff@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2008-10-01 03:54:53 +0000

    git-svn-id: http://mobicents.googlecode.com/svn/trunk@2988 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- servers/media/examples/mms-demo/slee/announcement-sbb/src/main/java/org/mobicents/examples/media/AnnouncementSbb.java
+++ servers/media/examples/mms-demo/slee/announcement-sbb/src/main/java/org/mobicents/examples/media/AnnouncementSbb.java
@@ -26,17 +26,21 @@ import javax.slee.SbbContext;
 import javax.slee.UnrecognizedActivityException;
 
 import org.apache.log4j.Logger;
-import org.mobicents.media.msc.common.MsLinkMode;
-import org.mobicents.media.server.impl.common.events.EventID;
 import org.mobicents.mscontrol.MsConnection;
 import org.mobicents.mscontrol.MsConnectionEvent;
 import org.mobicents.mscontrol.MsLink;
 import org.mobicents.mscontrol.MsLinkEvent;
+import org.mobicents.mscontrol.MsLinkMode;
 import org.mobicents.mscontrol.MsNotifyEvent;
 import org.mobicents.mscontrol.MsProvider;
 import org.mobicents.mscontrol.MsResource;
 import org.mobicents.mscontrol.MsSession;
-import org.mobicents.mscontrol.MsSignalGenerator;
+import org.mobicents.mscontrol.events.MsEventAction;
+import org.mobicents.mscontrol.events.MsEventFactory;
+import org.mobicents.mscontrol.events.MsRequestedEvent;
+import org.mobicents.mscontrol.events.MsRequestedSignal;
+import org.mobicents.mscontrol.events.ann.MsPlayRequestedSignal;
+import org.mobicents.mscontrol.events.pkg.MsAnnouncement;
 import org.mobicents.slee.resource.media.ratype.MediaRaActivityContextInterfaceFactory;
 
 /**
@@ -71,15 +75,17 @@ public abstract class AnnouncementSbb implements Sbb {
         try {
             linkActivity = mediaAcif.getActivityContextInterface(link);
         } catch (UnrecognizedActivityException ex) {
+            ex.printStackTrace();
+            return;
         }
 
         linkActivity.attach(sbbContext.getSbbLocalObject());
         link.join(userEndpoint, ANNOUNCEMENT_ENDPOINT);
     }
 
-    public void onLinkCreated(MsLinkEvent evt, ActivityContextInterface aci) {
+    public void onLinkConnected(MsLinkEvent evt, ActivityContextInterface aci) {
         MsLink link = evt.getSource();
-        String announcementEndpoint = link.getEndpoints()[1];
+        String announcementEndpoint = link.getEndpoints()[1].getLocalName();
 
         logger.info("Announcement endpoint: " + announcementEndpoint);
         this.setAnnouncementEndpoint(announcementEndpoint);
@@ -87,6 +93,20 @@ public abstract class AnnouncementSbb implements Sbb {
         playNext(link);
     }
 
+    public void onLinkDisconnected(MsLinkEvent evt, ActivityContextInterface aci) {
+        logger.info("Link release completed");
+
+        ActivityContextInterface connectionAci = getUserActivity();
+        if (connectionAci != null && !connectionAci.isEnding()) {
+            connectionAci.detach(sbbContext.getSbbLocalObject());
+            fireLinkDisconnected(evt, connectionAci, null);
+        }
+    }
+
+    public void onLinkFailed(MsLinkEvent evt, ActivityContextInterface aci) {
+        logger.error("Link failed");
+    }    
+    
     public void onAnnouncementComplete(MsNotifyEvent evt, ActivityContextInterface aci) {
         logger.info("Announcement complete: " + (this.getIndex() - 1));
         MsLink link = this.getLink();
@@ -107,24 +127,36 @@ public abstract class AnnouncementSbb implements Sbb {
 
     public void playNext(MsLink link) {
         String url = (String) this.getSequence().get(this.getIndex());
-        MsSignalGenerator generator = msProvider.getSignalGenerator(getAnnouncementEndpoint());
+        MsEventFactory eventFactory = msProvider.getEventFactory();
+        
+        MsPlayRequestedSignal play = null;
         try {
-            ActivityContextInterface generatorActivity = mediaAcif.getActivityContextInterface(generator);
-            generatorActivity.attach(sbbContext.getSbbLocalObject());
-            generator.apply(EventID.PLAY, link, new String[]{url});
-            this.setIndex(getIndex() + 1);
-        } catch (UnrecognizedActivityException e) {
+            play = (MsPlayRequestedSignal) 
+                eventFactory.createRequestedSignal(MsAnnouncement.PLAY);
+            play.setURL(url);
+            System.out.println("PLAY signal=" + play);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
         }
-    }
+        
+        MsRequestedEvent onCompleted = null;
+        MsRequestedEvent onFailed = null;
+        
+        try {
+            onCompleted = eventFactory.createRequestedEvent(MsAnnouncement.COMPLETED);
+            onCompleted.setEventAction(MsEventAction.NOTIFY);
 
-    public void onLinkReleased(MsLinkEvent evt, ActivityContextInterface aci) {
-        logger.info("Link release completed");
-
-        ActivityContextInterface connectionAci = getUserActivity();
-        if (connectionAci != null && !connectionAci.isEnding()) {
-            connectionAci.detach(sbbContext.getSbbLocalObject());
-            fireLinkReleased(evt, connectionAci, null);
+            onFailed = eventFactory.createRequestedEvent(MsAnnouncement.FAILED);
+            onFailed.setEventAction(MsEventAction.NOTIFY);
+        } catch (ClassNotFoundException e) {
         }
+        
+        MsRequestedSignal[] requestedSignals = new MsRequestedSignal[]{play};
+        MsRequestedEvent[] requestedEvents = new MsRequestedEvent[]{onCompleted, onFailed};
+        
+        System.out.println("EXECUTING PLAY");
+        link.getEndpoints()[1].execute(requestedSignals, requestedEvents, link);
+        setIndex(getIndex()+1);
     }
 
     public void onUserDisconnected(MsConnectionEvent evt, ActivityContextInterface aci) {
@@ -136,13 +168,14 @@ public abstract class AnnouncementSbb implements Sbb {
         }
 
         MsLink link = getLink();
+        System.out.println("Releasing link=" + link);
         if (link != null) {
             link.release();
         }
 
     }
 
-    public abstract void fireLinkReleased(MsLinkEvent evt, ActivityContextInterface aci, Address address);
+    public abstract void fireLinkDisconnected(MsLinkEvent evt, ActivityContextInterface aci, Address address);
 
     public MsLink getLink() {
         ActivityContextInterface[] activities = sbbContext.getActivities();
