ce09527b4de7f8c7eb1b014e9e14d4e5ae8ab3fd
amit.bhayani@gmail.com <amit.bhayani@gmail.com@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2008-10-06 13:35:14 +0000

    Added doco
    git-svn-id: http://mobicents.googlecode.com/svn/trunk@3063 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- servers/media/core/server-spi/src/main/java/org/mobicents/media/server/spi/EndpointQuery.java
+++ servers/media/core/server-spi/src/main/java/org/mobicents/media/server/spi/EndpointQuery.java
@@ -25,116 +25,131 @@ import javax.naming.InitialContext;
 import javax.naming.NamingEnumeration;
 import javax.naming.NamingException;
 
-//import org.mobicents.media.server.impl.BaseEndpoint;
 /**
- *
+ * The VirtualEndpoint like AnnEndpoint, PREndpoint, LoopEndpoint etc are
+ * registered with JNDI name as media/trunk/Announcement,
+ * media/trunk/PacketRelay, media/test/trunk/Loopback respectively. In case if
+ * the application wants to use new endpoint it can call lookup(endpointName)
+ * with wild-card '$'. For example media/trunk/Announcement/$. This will return
+ * a new VirtualEndpoint with specific name like media/trunk/Announcement/enp-2,
+ * application can store this endpoint name for latter use and call
+ * lookup("media/trunk/Announcement/enp-2") to get particular instance of
+ * Endpoint
+ * 
  * @author Oleg Kulikov
  */
 public class EndpointQuery {
 
-    protected static Logger logger = Logger.getLogger(EndpointQuery.class.getCanonicalName());
+	protected static Logger logger = Logger.getLogger(EndpointQuery.class.getCanonicalName());
 
-    public static synchronized Endpoint findAny(String name)
-            throws NamingException, ResourceUnavailableException {
-        InitialContext ic = new InitialContext();
+	public static synchronized Endpoint findAny(String name) throws NamingException, ResourceUnavailableException {
+		InitialContext ic = new InitialContext();
 
-        Object f = ic.lookup(name);
-        if (f instanceof VirtualEndpoint) {
-            return ((VirtualEndpoint) f).createEndpoint();
-        }
+		Object f = ic.lookup(name);
+		if (f instanceof VirtualEndpoint) {
+			return ((VirtualEndpoint) f).createEndpoint();
+		}
 
-        Context ctx = (Context) f;//ic.lookup(name);
+		Context ctx = (Context) f;// ic.lookup(name);
 
-        NamingEnumeration list = ctx.listBindings("");
-        while (list.hasMore()) {
+		NamingEnumeration list = ctx.listBindings("");
+		while (list.hasMore()) {
 
-            Binding binding = (Binding) list.next();
-            String bindingName = binding.getName();
+			Binding binding = (Binding) list.next();
 
-            Object obj = binding.getObject();
-            if (obj instanceof Endpoint) {
-                Endpoint endpoint = (Endpoint) obj;
-                if (!endpoint.hasConnections()) {
-                    return endpoint;
-                }
-            } else {
-                logger.info("Wrong class[" + obj.getClass() + "], it doesnt implement [" + Endpoint.class + "], if it does, its a class loader issue!!!");
-            }
-        }
+			Object obj = binding.getObject();
+			if (obj instanceof Endpoint) {
+				Endpoint endpoint = (Endpoint) obj;
+				if (!endpoint.hasConnections()) {
+					return endpoint;
+				}
+			} else {
+				logger.info("Wrong class[" + obj.getClass() + "], it doesnt implement [" + Endpoint.class
+						+ "], if it does, its a class loader issue!!!");
+			}
+		}
 
-        throw new ResourceUnavailableException("Endpoint unknown or in use[" + name + "]");
-    }
+		throw new ResourceUnavailableException("Endpoint unknown or in use[" + name + "]");
+	}
 
-    public static synchronized Collection findAll(String name) throws NamingException {
-        InitialContext ic = new InitialContext();
-        ArrayList endpoints = new ArrayList();
+	public static synchronized Collection findAll(String name) throws NamingException {
+		InitialContext ic = new InitialContext();
+		ArrayList endpoints = new ArrayList();
 
-        Context ctx = (Context) ic.lookup(name);
+		Context ctx = (Context) ic.lookup(name);
 
-        NamingEnumeration list = ctx.listBindings("");
-        while (list.hasMore()) {
-            Binding binding = (Binding) list.next();
+		NamingEnumeration list = ctx.listBindings("");
+		while (list.hasMore()) {
+			Binding binding = (Binding) list.next();
 
-            String bindingName = binding.getName();
-            Endpoint endpoint = (Endpoint) binding.getObject();
-            endpoints.add(endpoint);
-        }
+			String bindingName = binding.getName();
+			Endpoint endpoint = (Endpoint) binding.getObject();
+			endpoints.add(endpoint);
+		}
 
-        return endpoints;
-    }
+		return endpoints;
+	}
 
-    public static synchronized Endpoint find(String name)
-            throws NamingException, ResourceUnavailableException {
-        InitialContext ic = new InitialContext();
-        
-        EndpointName enpName = new EndpointName(name);
-        Object f = ic.lookup(enpName.getContextName());
-        if (f instanceof VirtualEndpoint) {
-            return ((VirtualEndpoint)f).getEndpoint(name);
-        }
-        
-        Endpoint endpoint = (Endpoint) ic.lookup(name);
-        return endpoint;
-    }
+	public static synchronized Endpoint find(String name) throws NamingException, ResourceUnavailableException {
+		InitialContext ic = new InitialContext();
 
-    public static Endpoint lookup(String name) throws NamingException, ResourceUnavailableException {
-        if (name.endsWith("/$")) {
-            name = name.substring(0, name.indexOf("/$"));
-            return findAny(name);
-        } else {
-            return find(name);
-        }
-    }
+		EndpointName enpName = new EndpointName(name);
+		Object f = ic.lookup(enpName.getContextName());
+		if (f instanceof VirtualEndpoint) {
+			return ((VirtualEndpoint) f).getEndpoint(name);
+		}
+
+		Endpoint endpoint = (Endpoint) ic.lookup(name);
+		return endpoint;
+	}
+
+	public static Endpoint lookup(String name) throws NamingException, ResourceUnavailableException {
+		if (name.endsWith("/$")) {
+			name = name.substring(0, name.indexOf("/$"));
+			return findAny(name);
+		} else {
+			return find(name);
+		}
+	}
 }
 
+/**
+ * 
+ * EndpointName breaks the JNDI name of Endpoint into context and actual name.
+ * For example for jndiname "media/trunk/Conference/enp-1" the contextName is
+ * "media/trunk/Conference" and name is "enp-1"
+ * 
+ * @author Oleg Kulikov
+ * 
+ */
 class EndpointName {
 
-    private String contextName;
-    private String name;
+	private String contextName;
+	private String name;
 
-    public EndpointName(String fqn) {
-        parse(fqn);
-    }
+	public EndpointName(String fqn) {
+		parse(fqn);
+	}
 
-    public String getContextName() {
-        return contextName;
-    }
+	public String getContextName() {
+		return contextName;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    private void parse(String fqn) {
-        int pos = 0;
-        int idx = 0;
-        while (idx != -1) {
-            idx = fqn.indexOf("/", pos);
-            if (idx != -1) {
-                pos = idx + 1;
-            }
-        }
+	private void parse(String fqn) {
+		int pos = 0;
+		int idx = 0;
+		while (idx != -1) {
+			idx = fqn.indexOf("/", pos);
+			if (idx != -1) {
+				pos = idx + 1;
+			}
+		}
 
-        contextName = fqn.substring(0, pos - 1).trim();
-        name = fqn.substring(pos).trim();
-    }
+		contextName = fqn.substring(0, pos - 1).trim();
+		name = fqn.substring(pos).trim();
+	}
 }
