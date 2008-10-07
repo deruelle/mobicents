cb1c58aa53f4c4fa1481029e73d5f8edb2999d05
amit.bhayani@gmail.com <amit.bhayani@gmail.com@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2008-10-07 10:12:27 +0000

    Fix for issue http://code.google.com/p/mobicents/issues/detail?id=380 NPE at MsLinkEventProxy, when Link is disconnected. Added a check for Session not null.
    git-svn-id: http://mobicents.googlecode.com/svn/trunk@3075 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- servers/media/controllers/msc/ra/src/main/java/org/mobicents/slee/resource/media/local/MsLinkEventProxy.java
+++ servers/media/controllers/msc/ra/src/main/java/org/mobicents/slee/resource/media/local/MsLinkEventProxy.java
@@ -10,55 +10,62 @@ import org.mobicents.mscontrol.MsLinkEvent;
 import org.mobicents.mscontrol.MsLinkListener;
 
 /**
- *
+ * 
  * @author Oleg Kulikov
  */
 public class MsLinkEventProxy implements MsLinkListener {
 
-    private MsProviderLocal provider;
-    
-    protected MsLinkEventProxy(MsProviderLocal provider) {
-        this.provider = provider;
-    }
-    
-    public void linkCreated(MsLinkEvent evt) {
-        MsSessionLocal session = (MsSessionLocal) provider.sessions.get(evt.getSource().getSession().getId());
-        MsLink link = new MsLinkLocal(session, evt.getSource());
-        session.links.put(link.getId(), link);
-        
-        
-        MsLinkEventLocal event = new MsLinkEventLocal(evt, link);
-        for (MsLinkListener listener : provider.linkListeners) {
-            listener.linkCreated(event);
-        }
-        session.linkActivityCreated();
-    }
+	private MsProviderLocal provider;
 
-    public void linkConnected(MsLinkEvent evt) {
-        MsSessionLocal session = (MsSessionLocal) provider.sessions.get(evt.getSource().getSession().getId());
-        MsLink link = (MsLink) session.links.get(evt.getSource().getId());
-        MsLinkEventLocal event = new MsLinkEventLocal(evt, link);
-        for (MsLinkListener listener : provider.linkListeners) {
-            listener.linkConnected(event);
-        }
-    }
+	protected MsLinkEventProxy(MsProviderLocal provider) {
+		this.provider = provider;
+	}
 
-    public void linkDisconnected(MsLinkEvent evt) {
-        MsSessionLocal session = (MsSessionLocal) provider.sessions.get(evt.getSource().getSession().getId());
-        MsLink link = (MsLink) session.links.remove(evt.getSource().getId());
-        MsLinkEventLocal event = new MsLinkEventLocal(evt, link);
-        for (MsLinkListener listener : provider.linkListeners) {
-            listener.linkDisconnected(event);
-        }
-    }
+	public void linkCreated(MsLinkEvent evt) {
+		MsSessionLocal session = (MsSessionLocal) provider.sessions.get(evt.getSource().getSession().getId());
+		MsLink link = new MsLinkLocal(session, evt.getSource());
+		session.links.put(link.getId(), link);
 
-    public void linkFailed(MsLinkEvent evt) {
-        MsSessionLocal session = (MsSessionLocal) provider.sessions.get(evt.getSource().getSession().getId());
-        MsLink link = (MsLink) session.links.get(evt.getSource().getId());
-        MsLinkEventLocal event = new MsLinkEventLocal(evt, link);
-        for (MsLinkListener listener : provider.linkListeners) {
-            listener.linkFailed(event);
-        }
-    }
+		MsLinkEventLocal event = new MsLinkEventLocal(evt, link);
+		for (MsLinkListener listener : provider.linkListeners) {
+			listener.linkCreated(event);
+		}
+		session.linkActivityCreated();
+	}
+
+	public void linkConnected(MsLinkEvent evt) {
+		MsSessionLocal session = (MsSessionLocal) provider.sessions.get(evt.getSource().getSession().getId());
+		MsLink link = (MsLink) session.links.get(evt.getSource().getId());
+		MsLinkEventLocal event = new MsLinkEventLocal(evt, link);
+		for (MsLinkListener listener : provider.linkListeners) {
+			listener.linkConnected(event);
+		}
+	}
+
+	public void linkDisconnected(MsLinkEvent evt) {
+		String eventSessionId = evt.getSource().getSession().getId();
+		MsSessionLocal session = (MsSessionLocal) provider.sessions.get(eventSessionId);
+		MsLink link = null;
+		if (session != null) {
+			link = (MsLink) session.links.remove(evt.getSource().getId());
+		} else {
+			// Probably the session is already killed and hence it could be null
+			link = evt.getSource();
+		}
+
+		MsLinkEventLocal event = new MsLinkEventLocal(evt, link);
+		for (MsLinkListener listener : provider.linkListeners) {
+			listener.linkDisconnected(event);
+		}
+	}
+
+	public void linkFailed(MsLinkEvent evt) {
+		MsSessionLocal session = (MsSessionLocal) provider.sessions.get(evt.getSource().getSession().getId());
+		MsLink link = (MsLink) session.links.get(evt.getSource().getId());
+		MsLinkEventLocal event = new MsLinkEventLocal(evt, link);
+		for (MsLinkListener listener : provider.linkListeners) {
+			listener.linkFailed(event);
+		}
+	}
 
 }
