e788543ee6737254cc5c2d43653cacd64b645e45
oleg.kulikoff <oleg.kulikoff@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2008-10-15 06:59:47 +0000

    Memory leak fixed
    git-svn-id: http://mobicents.googlecode.com/svn/trunk@3229 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- servers/media/controllers/msc/ra/src/main/java/org/mobicents/slee/resource/media/local/MsSessionLocal.java
+++ servers/media/controllers/msc/ra/src/main/java/org/mobicents/slee/resource/media/local/MsSessionLocal.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.mobicents.slee.resource.media.local;
 
 import EDU.oswego.cs.dl.util.concurrent.ConcurrentReaderHashMap;
@@ -25,107 +24,105 @@ import org.mobicents.mscontrol.MsSessionState;
  * @author amit.bhayani
  */
 public class MsSessionLocal implements MsSession {
-	private MsSession session;
-	private MsProviderLocal provider;
 
-	protected ConcurrentReaderHashMap connections = new ConcurrentReaderHashMap();
-	protected ConcurrentReaderHashMap links = new ConcurrentReaderHashMap();
+    private MsSession session;
+    private MsProviderLocal provider;
+    protected ConcurrentReaderHashMap connections = new ConcurrentReaderHashMap();
+    protected ConcurrentReaderHashMap links = new ConcurrentReaderHashMap();
+    private ReentrantLock blockState = new ReentrantLock();
+    private Condition connectionActivityCreated = blockState.newCondition();
+    private Condition linkActivityCreated = blockState.newCondition();
 
-	private ReentrantLock blockState = new ReentrantLock();
-	private Condition connectionActivityCreated = blockState.newCondition();
-	private Condition linkActivityCreated = blockState.newCondition();
+    protected MsSessionLocal(MsSession session, MsProviderLocal provider) {
+        this.session = session;
+        this.provider = provider;
+    }
 
-	protected MsSessionLocal(MsSession session, MsProviderLocal provider) {
-		this.session = session;
-		this.provider = provider;
-	}
+    public String getId() {
+        return session.getId();
+    }
 
-	public String getId() {
-		return session.getId();
-	}
+    public MsProvider getProvider() {
+        return provider;
+    }
 
-	public MsProvider getProvider() {
-		return provider;
-	}
+    public MsSessionState getState() {
+        return session.getState();
+    }
 
-	public MsSessionState getState() {
-		return session.getState();
-	}
+    public MsConnection createNetworkConnection(String endpointName) {
+        blockState.lock();
+        try {
+            MsConnection connection = session.createNetworkConnection(endpointName);
+            while (!connections.containsKey(connection.getId())) {
+                try {
+                    connectionActivityCreated.await();
+                } catch (InterruptedException e) {
+                    connection.release();
+                    return null;
+                }
+            }
+            return (MsConnection) connections.get(connection.getId());
+        } finally {
+            blockState.unlock();
+        }
+    }
 
-	public MsConnection createNetworkConnection(String endpointName) {
-		blockState.lock();
-		try {
-			MsConnection connection = session.createNetworkConnection(endpointName);
-			while (!connections.containsKey(connection.getId())) {
-				try {
-					connectionActivityCreated.await();
-				} catch (InterruptedException e) {
-					connection.release();
-					return null;
-				}
-			}
-			return (MsConnection) connections.get(connection.getId());
-		} finally {
-			blockState.unlock();
-		}
-	}
+    public MsLink createLink(MsLinkMode mode) {
+        blockState.lock();
+        try {
+            MsLink link = session.createLink(mode);
+            while (!links.containsKey(link.getId())) {
+                try {
+                    linkActivityCreated.await();
+                } catch (InterruptedException e) {
+                    link.release();
+                    return null;
+                }
+            }
+            return (MsLink) links.get(link.getId());
+        } finally {
+            blockState.unlock();
+        }
+    }
 
-	public MsLink createLink(MsLinkMode mode) {
-		blockState.lock();
-		try {
-			MsLink link = session.createLink(mode);
-			while (!links.containsKey(link.getId())) {
-				try {
-					linkActivityCreated.await();
-				} catch (InterruptedException e) {
-					link.release();
-					return null;
-				}
-			}
-			return (MsLink) links.get(link.getId());
-		} finally {
-			blockState.unlock();
-		}
-	}
+    public void addSessionListener(MsSessionListener listener) {
+        throw new SecurityException("addSessionListener is unsupported. Use event handlers of SBB");
+    }
 
-	public void addSessionListener(MsSessionListener listener) {
-		throw new SecurityException("addSessionListener is unsupported. Use event handlers of SBB");
-	}
+    public void removeSessionListener(MsSessionListener listener) {
+        throw new SecurityException("removeSessionListener is unsupported.");
+    }
 
-	public void removeSessionListener(MsSessionListener listener) {
-		throw new SecurityException("removeSessionListener is unsupported.");
-	}
+    public List<MsConnection> getConnections() {
+        Collection<MsConnection> values = connections.values();
+        ArrayList list = new ArrayList();
+        for (MsConnection connection : values) {
+            list.add(connection);
+        }
+        return list;
+    }
 
-	public List<MsConnection> getConnections() {
-		Collection<MsConnection> values = connections.values();
-		ArrayList list = new ArrayList();
-		for (MsConnection connection : values) {
-			list.add(connection);
-		}
-		return list;
-	}
+    public void connectionActivityCreated() {
+        blockState.lock();
+        try {
+            this.connectionActivityCreated.signalAll();
+        } finally {
+            blockState.unlock();
+        }
+    }
 
-	public void connectionActivityCreated() {
-		blockState.lock();
-		try {
-			this.connectionActivityCreated.signalAll();
-		} finally {
-			blockState.unlock();
-		}
-	}
+    public void linkActivityCreated() {
+        blockState.lock();
+        try {
+            this.linkActivityCreated.signalAll();
+        } finally {
+            blockState.unlock();
+        }
+    }
 
-	public void linkActivityCreated() {
-		blockState.lock();
-		try {
-			this.linkActivityCreated.signalAll();
-		} finally {
-			blockState.unlock();
-		}
-	}
-
-	@Override
-	public String toString() {
-		return session.toString();
-	}
-
+    @Override
+    public String toString() {
+        return session.toString();
+    }
 }
