589792de99c504679060955805993198a7d31abc
baranowb <baranowb@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2008-11-14 17:03:08 +0000

    Bug reported by Amit. Caused by typo, tearDown used jboss mbean stop - which destroyed mbean, instead of endpoint stop, which makes it not accept new reqeusts. tearDown caused whole MMS to be expunched.
    git-svn-id: http://mobicents.googlecode.com/svn/trunk@3672 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- servers/media/core/server-impl/src/main/java/org/mobicents/media/server/impl/jmx/EndpointManagement.java
+++ servers/media/core/server-impl/src/main/java/org/mobicents/media/server/impl/jmx/EndpointManagement.java
@@ -344,7 +344,7 @@ public abstract class EndpointManagement extends ServiceMBeanSupport implements 
 	}
 
 	public void tearDownPlatform() {
-		this.stop();
+		this.stopPlatform();
 		for(String name: this.getEndpointNames())
 		{
 			this.destroyEndpoint(name);
