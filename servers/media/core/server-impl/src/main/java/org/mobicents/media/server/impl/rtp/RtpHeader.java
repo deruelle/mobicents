822fb6cc0bc658b5e888cd4ac7c6fc2b71f058d5
oleg.kulikoff <oleg.kulikoff@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2009-01-06 14:41:30 +0000

    Issue #501
    git-svn-id: http://mobicents.googlecode.com/svn/trunk@4175 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- servers/media/core/server-impl/src/main/java/org/mobicents/media/server/impl/rtp/RtpHeader.java
+++ servers/media/core/server-impl/src/main/java/org/mobicents/media/server/impl/rtp/RtpHeader.java
@@ -44,11 +44,61 @@ public class RtpHeader implements Serializable {
     private int timestamp;
     private long ssrc;
 
-    public RtpHeader(byte payloadType, int seqNumber, int timestamp, long ssrc) {
+    private byte[] bin = new byte[12];
+    
+    public void RtpHeader() {
+    }
+    
+    public void init(byte[] bin) {
+        int b = bin[0] & 0xff;
+        version = (b & 0x0C) >> 6;
+        padding = (b & 0x20) == 0x020;
+        extensions = (b & 0x10) == 0x10;
+        cc = b & 0x0F;
+
+        b = bin[1] & 0xff;
+
+        marker = (b & 0x80) == 0x80;
+        payloadType = b & 0x7F;
+        
+        seqNumber = (bin[2] & 0xff) << 8;
+        seqNumber = seqNumber | (bin[3] & 0xff);
+
+        int p = (bin[4] << 24);
+        int q = (bin[5] << 16);
+        int r = (bin[6] << 8);
+        int s = (bin[7]);
+        
+        timestamp = p|q|r|s;
+        
+        p = (bin[4] << 24);
+        q = (bin[5] << 16);
+        r = (bin[6] << 8);
+        s = (bin[7]);
+        
+        ssrc = p|q|r|s;
+    }
+    
+    public void init(byte payloadType, int seqNumber, int timestamp, long ssrc) {
         this.payloadType = payloadType;
         this.seqNumber = seqNumber;
         this.timestamp = timestamp;
         this.ssrc = ssrc;
+        
+        bin[0]=(byte)(0x80);
+        bin[1]=(payloadType);
+        bin[2]=((byte) ((seqNumber & 0xFF00) >> 8));
+        bin[3]=((byte) (seqNumber & 0x00FF));
+
+        bin[4]=((byte) ((timestamp & 0xFF000000) >> 24));
+        bin[5]=((byte) ((timestamp & 0x00FF0000) >> 16));
+        bin[6]=((byte) ((timestamp & 0x0000FF00) >> 8));
+        bin[7]=((byte) ((timestamp & 0x000000FF)));
+
+        bin[8]=((byte) ((ssrc & 0xFF000000) >> 24));
+        bin[9]=((byte) ((ssrc & 0x00FF0000) >> 16));
+        bin[10]=((byte) ((ssrc & 0x0000FF00) >> 8));
+        bin[11]=((byte) ((ssrc & 0x000000FF)));
     }
     
     public int getPayloadType() {
@@ -63,4 +113,7 @@ public class RtpHeader implements Serializable {
         return timestamp;
     }
     
+    public byte[] toByteArray() {
+        return bin;
+    }
 }
