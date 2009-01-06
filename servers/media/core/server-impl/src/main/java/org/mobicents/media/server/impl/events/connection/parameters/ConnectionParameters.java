4ce28ab7d734cb2a6e40e5424a56e226c3352cf9
oleg.kulikoff <oleg.kulikoff@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2009-01-06 14:32:31 +0000

    Issue #501
    git-svn-id: http://mobicents.googlecode.com/svn/trunk@4167 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- servers/media/core/server-impl/src/main/java/org/mobicents/media/server/impl/events/connection/parameters/ConnectionParameters.java
+++ servers/media/core/server-impl/src/main/java/org/mobicents/media/server/impl/events/connection/parameters/ConnectionParameters.java
@@ -3,7 +3,7 @@ package org.mobicents.media.server.impl.events.connection.parameters;
 import org.mobicents.media.server.impl.AbstractSignal;
 import org.mobicents.media.server.impl.BaseConnection;
 import org.mobicents.media.server.impl.BaseEndpoint;
-import org.mobicents.media.server.impl.Generator;
+import org.mobicents.media.server.impl.MediaResource;
 import org.mobicents.media.server.impl.events.announcement.AudioPlayer;
 
 
@@ -12,10 +12,10 @@ public class ConnectionParameters extends AbstractSignal {
 	@Override
 	public void apply(BaseConnection connection) {
 		
-		BaseEndpoint endpoint = (BaseEndpoint) connection.getEndpoint();
-        ConnectionParametersGenerator player = (ConnectionParametersGenerator) endpoint.getMediaSource(Generator.CONNECTION_PARAMETERS, connection);
+//		BaseEndpoint endpoint = (BaseEndpoint) connection.getEndpoint();
+//        ConnectionParametersGenerator player = (ConnectionParametersGenerator) endpoint.getMediaSource(MediaResource.CONNECTION_PARAMETERS, connection);
         //now lets hit players button
-		player.generateParametersEvent(connection);
+//		player.generateParametersEvent(connection);
 	}
 
 	@Override
@@ -23,5 +23,10 @@ public class ConnectionParameters extends AbstractSignal {
 		
 		
 	}
+
+    @Override
+    public void cancel() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
 }
