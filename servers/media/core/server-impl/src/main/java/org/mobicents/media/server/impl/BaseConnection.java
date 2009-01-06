0313487462b48d9f5608307f50106ce325642edd
oleg.kulikoff <oleg.kulikoff@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2009-01-06 14:20:37 +0000

    Issue #501
    git-svn-id: http://mobicents.googlecode.com/svn/trunk@4148 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- servers/media/core/server-impl/src/main/java/org/mobicents/media/server/impl/BaseConnection.java
+++ servers/media/core/server-impl/src/main/java/org/mobicents/media/server/impl/BaseConnection.java
@@ -13,17 +13,12 @@
  */
 package org.mobicents.media.server.impl;
 
-import java.util.ArrayList;
+import java.util.List;
 import java.util.TimerTask;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.locks.ReentrantLock;
-import org.apache.log4j.Logger;
 import org.jboss.util.id.UID;
 import org.mobicents.media.server.local.management.ConnectionLocalManagement;
-import org.mobicents.media.server.local.management.WorkDataGatherer;
 import org.mobicents.media.server.spi.Connection;
 import org.mobicents.media.server.spi.ConnectionListener;
 import org.mobicents.media.server.spi.ConnectionMode;
@@ -38,54 +33,29 @@ import org.mobicents.media.server.spi.events.RequestedEvent;
  * 
  * @author Oleg Kulikov
  */
-public abstract class BaseConnection implements Connection, NotificationListener, ConnectionLocalManagement, WorkDataGatherer {
+public abstract class BaseConnection implements Connection, NotificationListener, ConnectionLocalManagement {
 
     protected String id;
+    private int index;
+    
     protected BaseEndpoint endpoint;
     protected String endpointName;
-    
     protected ConnectionMode mode;
-    protected ConnectionState state = ConnectionState.NULL;
-    
+    protected ConnectionState state;
     private int lifeTime = 30;
-    
-    protected transient Multiplexer mux;
-    protected transient Demultiplexer demux;
-    
     private boolean timerStarted = false;
-    
     private ReentrantLock stateLock = new ReentrantLock();
+    private List<ConnectionListener> listeners = new CopyOnWriteArrayList();
+    private List<RequestedEvent> eventListeners = new CopyOnWriteArrayList();
     
-    private ArrayList<ConnectionListener> listeners = new ArrayList();
-    private ArrayList<RequestedEvent> eventListeners = new ArrayList();
-    
-    private transient ExecutorService eventQueue = Executors.newSingleThreadExecutor(new BaseConnection.ThreadFactoryImpl());
-    
-    protected transient Logger logger = Logger.getLogger(BaseConnection.class);
     private transient TimerTask closeTask;
-    
     private long connectionCreationTime = System.currentTimeMillis();
+    private long packets;
     
-    //http://tools.ietf.org/html/rfc3435#section-3.2.2
-    //http://tools.ietf.org/html/rfc3435#section-3.2.2.7 - unsigned integers, up to nine digits
-    protected int packetsSent=0;
-    protected int packetsReceived=0;
-    protected int octetsReceived=0;
-    protected int octetsSent=0;
-    protected int interArrivalJitter=0;
-    protected int packetsLost=0;
-
-    
-    
-    protected  boolean gatherStats=false;
     private class CloseConnectionTask extends TimerTask {
-
         public void run() {
-            logger.info("Connection timer expired, Disconnecting");
             timerStarted = false;
             endpoint.deleteConnection(id);
-
-            eventQueue.shutdown();
         }
     }
 
@@ -96,9 +66,12 @@ public abstract class BaseConnection implements Connection, NotificationListener
         this.endpoint = (BaseEndpoint) endpoint;
         this.endpointName = endpoint.getLocalName();
 
-        mux = new Multiplexer();
-        demux = new Demultiplexer(this.endpoint.getSupportedFormats());
-
+        this.endpoint.connections.put(id, this);
+        this.index = this.endpoint.getIndex();
+        setState(ConnectionState.NULL);
+        
+        this.endpoint.allocateMediaSinks(this);        
+        
         setLifeTime(lifeTime);
         setState(ConnectionState.NULL);
     }
@@ -112,6 +85,10 @@ public abstract class BaseConnection implements Connection, NotificationListener
         return id;
     }
 
+    public int getIndex() {
+        return index;
+    }
+    
     /**
      * Gets the current state of this connection.
      * 
@@ -137,14 +114,6 @@ public abstract class BaseConnection implements Connection, NotificationListener
      */
     public void setLifeTime(int lifeTime) {
         this.lifeTime = lifeTime;
-        if (timerStarted) {
-            closeTask.cancel();
-            BaseEndpoint.connectionTimer.purge();
-            timerStarted = false;
-        }
-        this.closeTask = new CloseConnectionTask();
-        BaseEndpoint.connectionTimer.schedule(closeTask, 60 * lifeTime * 1000);
-        timerStarted = true;
     }
 
     /**
@@ -174,31 +143,16 @@ public abstract class BaseConnection implements Connection, NotificationListener
         if (mode == this.mode) {
             return;
         }
-        
-        ConnectionMode oldMode = this.mode;        
+
+        ConnectionMode oldMode = this.mode;
         this.mode = mode;
-        
-        if (mode == ConnectionMode.RECV_ONLY) {
-            getMux().getOutput().stop();
-            getDemux().start();
-        } else if (mode == ConnectionMode.SEND_ONLY) {
-            getDemux().stop();
-            getMux().getOutput().start();
-        } else {
-            getMux().getOutput().start();
-            getDemux().start();
-        }
-        
+
+        //@TODO disbale receiver/sender
         endpoint.notifyEndpoint(this, oldMode);
-        
-        //for (ConnectionListener cl : endpoint.connectionListeners) {
-        //    cl.onModeChange(this, oldMode);
-        //}
-        
         for (ConnectionListener cl : listeners) {
             cl.onModeChange(this, oldMode);
         }
-        
+
     }
 
     /**
@@ -210,14 +164,6 @@ public abstract class BaseConnection implements Connection, NotificationListener
         return endpoint;
     }
 
-    public Multiplexer getMux() {
-        return mux;
-    }
-
-    public Demultiplexer getDemux() {
-        return demux;
-    }
-
     /**
      * Adds listener for the specified event.
      * 
@@ -225,12 +171,10 @@ public abstract class BaseConnection implements Connection, NotificationListener
      *            the event to detect or null to diable detection.
      */
     public void detect(RequestedEvent requestedEvent) {
-        synchronized (eventListeners) {
-            if (requestedEvent != null) {
-                eventListeners.add(requestedEvent);
-            } else {
-                eventListeners.clear();
-            }
+        if (requestedEvent != null) {
+            eventListeners.add(requestedEvent);
+        } else {
+            eventListeners.clear();
         }
     }
 
@@ -253,16 +197,19 @@ public abstract class BaseConnection implements Connection, NotificationListener
     }
 
     protected void setState(ConnectionState newState) {
-
         if (newState == this.state) {
             return;
         }
-        
+
         ConnectionState oldState = this.state;
         this.state = newState;
 
 
- 
+        if (state != ConnectionState.CLOSED) {
+            int timeout = getTimeout(newState);
+            scheduleTermination(timeout);
+        }
+
         endpoint.notifyEndpoint(this, oldState);
         for (ConnectionListener cl : listeners) {
             cl.onStateChange(this, oldState);
@@ -271,11 +218,9 @@ public abstract class BaseConnection implements Connection, NotificationListener
     }
 
     public void update(NotifyEvent event) {
-        synchronized (eventListeners) {
-            for (RequestedEvent request : this.eventListeners) {
-                if (request.getID().equals(event.getEventID())) {
-                    eventQueue.submit(new EventSender(request.getHandler(), event));
-                }
+        for (RequestedEvent request : this.eventListeners) {
+            if (request.getID().equals(event.getEventID())) {
+                endpoint.sendEvent(new EventSender(request.getHandler(), event));
             }
         }
     }
@@ -294,15 +239,56 @@ public abstract class BaseConnection implements Connection, NotificationListener
      * 
      * @throws InterruptedException
      */
-    public void close() {
+    protected void close() {
         if (timerStarted) {
             closeTask.cancel();
             timerStarted = false;
-            BaseEndpoint.connectionTimer.purge();
+            BaseEndpoint.stateTimer.purge();
+        }
+        
+        endpoint.releaseIndex(index);
+        endpoint.releaseMediaSinks(this);
+        endpoint.releaseMediaSources(this);
+        
+        try {
+            this.setState(ConnectionState.CLOSED);
+        } finally {
+            endpoint.connections.remove(id);
+        }
+    }
+
+    /**
+     * Gets time out for specified state.
+     * 
+     * @param state the state identifier
+     * @return the value of timeout in seconds
+     */
+    private int getTimeout(ConnectionState state) {
+        switch (state) {
+            case OPEN:
+                return this.endpoint.connectionLifeTime * 60;
+            default:
+                return 10;
+        }
+    }
+
+    /**
+     * Schedules connection termination due to timeout or lifetime expire
+     * 
+     * @param timeout the value of timeout in seconds
+     */
+    private void scheduleTermination(int timeout) {
+        //cancel previous termination task if started
+        if (timerStarted) {
+            closeTask.cancel();
+            BaseEndpoint.stateTimer.purge();
+            timerStarted = false;
         }
 
-        this.setState(ConnectionState.CLOSED);
-        eventQueue.shutdown();
+        //schedule new termination task
+        this.closeTask = new CloseConnectionTask();
+        BaseEndpoint.stateTimer.schedule(closeTask, timeout * 1000);
+        timerStarted = true;
     }
 
     public void lockState() throws InterruptedException {
@@ -313,127 +299,13 @@ public abstract class BaseConnection implements Connection, NotificationListener
         this.stateLock.unlock();
     }
 
-    private static class ThreadFactoryImpl implements ThreadFactory {
+   
 
-        final ThreadGroup group;
-        static final AtomicInteger baseConnectionPoolNumber = new AtomicInteger(1);
-        final AtomicInteger threadNumber = new AtomicInteger(1);
-        final String namePrefix;
-
-        ThreadFactoryImpl() {
-            SecurityManager s = System.getSecurityManager();
-            group = (s != null) ? s.getThreadGroup() : Thread.currentThread().getThreadGroup();
-            namePrefix = "BaseConnection-SingleThreadExecutor-" + baseConnectionPoolNumber.getAndIncrement() + "thread-";
-        }
-
-        public Thread newThread(Runnable r) {
-            Thread t = new Thread(group, r, namePrefix + threadNumber.getAndIncrement(), 0);
-            if (t.isDaemon()) {
-                t.setDaemon(false);
-            }
-            if (t.getPriority() != Thread.NORM_PRIORITY) {
-                t.setPriority(Thread.NORM_PRIORITY);
-            }
-            return t;
-        }
-    }
-    
-    //  ////////////////
-    //  // STATISTICS //
-    //  ////////////////
-    
-    
-    
     public long getConnectionCreationTime() throws IllegalArgumentException {
-
         return connectionCreationTime;
     }
 
-    public boolean isGatherStats() {
-		return gatherStats;
-	}
-
-	
-
-	public int getPacketsSent() {
-		return packetsSent;
-	}
-
-	public int getPacketsReceived() {
-		return packetsReceived;
-	}
-
-	public int getOctetsReceived() {
-		return octetsReceived;
-	}
-
-	public int getOctetsSent() {
-		return octetsSent;
-	}
-
-	public int getInterArrivalJitter() {
-		return interArrivalJitter;
-	}
-
-	public int getPacketsLost() {
-		return packetsLost;
-	}
-
-	
-	private final static int _DATA_LIMIT=1000000000;
-	// ///////////////////
-	// // Data gatherer //
-	// ///////////////////
-	public void octetsReceived(int count) {
-		this.octetsReceived+=count;
-		if(this.octetsReceived>_DATA_LIMIT)
-		{
-			this.octetsReceived=_DATA_LIMIT;
-		}
-		
-	}
-
-	public void octetsSent(int count) {
-		this.octetsSent+=count;
-		if(this.octetsSent>_DATA_LIMIT)
-		{
-			this.octetsSent=_DATA_LIMIT;
-		}
-		
-	}
-
-	public void packetsLost(int count) {
-		this.packetsLost+=count;
-		if(this.packetsLost>_DATA_LIMIT)
-		{
-			this.packetsLost=_DATA_LIMIT;
-		}
-		
-	}
-
-	public void packetsReceived(int count) {
-		this.packetsReceived+=count;
-		if(this.packetsReceived>_DATA_LIMIT)
-		{
-			this.packetsReceived=_DATA_LIMIT;
-		}
-		
-	}
-
-	public void packetsSent(int count) {
-		this.packetsSent+=count;
-		if(this.packetsSent>_DATA_LIMIT)
-		{
-			this.packetsSent=_DATA_LIMIT;
-		}
-		
-	}
-
-	public void interArrivalJitter(int value) {
-		//Its a simple set
-		this.interArrivalJitter=value;
-	}
-	
-	
-    
+    public long getNumberOfPackets() throws IllegalArgumentException {
+        return packets;
+    }
 }
