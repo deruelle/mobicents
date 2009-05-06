7d78857a048eb80d434d13804a79a471a86d7c15
oleg.kulikoff <oleg.kulikoff@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2009-05-06 04:26:34 +0000

    Add DtmfEvent
    git-svn-id: http://mobicents.googlecode.com/svn/trunk@5856 bf0df8d0-2c1f-0410-b170-bd30377b63dc

---
+++ servers/media/core/server-impl/src/main/java/org/mobicents/media/server/impl/events/dtmf/DtmfEvent.java
@@ -0,0 +1,76 @@
+/*
+ * Mobicents, Communications Middleware
+ * 
+ * Copyright (c) 2008, Red Hat Middleware LLC or third-party
+ * contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat Middleware LLC.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ *
+ * Boston, MA  02110-1301  USA
+ */
+package org.mobicents.media.server.impl.events.dtmf;
+
+import org.mobicents.media.server.spi.Connection;
+import org.mobicents.media.server.spi.Endpoint;
+import org.mobicents.media.server.spi.events.NotifyEvent;
+
+/**
+ *
+ * @author kulikov
+ */
+public class DtmfEvent implements NotifyEvent {
+    public final static int DTMF_0 = 0;
+    public final static int DTMF_1 = 1;
+    public final static int DTMF_2 = 2;
+    public final static int DTMF_3 = 3;
+    public final static int DTMF_4 = 4;
+    public final static int DTMF_5 = 5;
+    public final static int DTMF_6 = 6;
+    public final static int DTMF_7 = 7;
+    public final static int DTMF_8 = 8;
+    public final static int DTMF_9 = 9;
+    public final static int DTMF_HASH = 10;
+    public final static int DTMF_STAR = 11;
+    
+    private Endpoint endpoint;
+    private Connection connection;
+    private int resourceID;
+    private int eventID;
+    
+    public DtmfEvent(Endpoint endpoint, Connection connection, int eventID) {
+        
+    }
+    
+    public Endpoint getEndpoint() {
+        return endpoint;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public int getResourceID() {
+        return -1;
+    }
+
+    public int getEventID() {
+        return eventID;
+    }
+
+}
