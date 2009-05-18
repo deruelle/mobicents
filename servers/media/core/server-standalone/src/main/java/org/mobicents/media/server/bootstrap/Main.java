25f191550c6c3d1fe0b6fb07ef03574484c8a54f
amit.bhayani@gmail.com <amit.bhayani@gmail.com@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2009-05-18 11:48:14 +0000

    git-svn-id: http://mobicents.googlecode.com/svn/trunk@6024 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- servers/media/core/server-standalone/src/main/java/org/mobicents/media/server/bootstrap/Main.java
+++ servers/media/core/server-standalone/src/main/java/org/mobicents/media/server/bootstrap/Main.java
@@ -60,7 +60,7 @@ public class Main {
 		String homeDir = getHomeDir(args);
 		System.setProperty(MMS_HOME, homeDir);
 
-		String Log4jURL = homeDir +"/"+ LOG4J_URL;
+		String Log4jURL = homeDir + LOG4J_URL;
 		
 		URL log4jurl = getURL(Log4jURL);		
 		InputStream inStreamLog4j = log4jurl.openStream();
