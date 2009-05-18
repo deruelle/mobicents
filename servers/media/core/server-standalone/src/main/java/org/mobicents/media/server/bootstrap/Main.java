ca373472413d9f1f23fa4c8751d10ae7d1a10edb
amit.bhayani@gmail.com <amit.bhayani@gmail.com@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2009-05-18 09:23:27 +0000

    Added code for configuration of log4j
    git-svn-id: http://mobicents.googlecode.com/svn/trunk@6021 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- servers/media/core/server-standalone/src/main/java/org/mobicents/media/server/bootstrap/Main.java
+++ servers/media/core/server-standalone/src/main/java/org/mobicents/media/server/bootstrap/Main.java
@@ -22,9 +22,13 @@
 package org.mobicents.media.server.bootstrap;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import java.util.Properties;
 
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.jboss.dependency.spi.Controller;
 import org.jboss.dependency.spi.ControllerContext;
 import org.jboss.kernel.Kernel;
@@ -38,109 +42,127 @@ import org.jboss.util.StringPropertyReplacer;
  */
 public class Main {
 
-    private final static String HOME_DIR = "MMS_HOME";
-    private final static String BOOT_URL = "/conf/bootstrap-beans.xml";
-    public static final String MMS_HOME = "mms.home.dir";
-    
-    private static int index = 0;
-    
-    
-    private Kernel kernel;
-    private BasicXMLDeployer kernelDeployer;
-    private Controller controller;
+	private final static String HOME_DIR = "MMS_HOME";
+	private final static String BOOT_URL = "/conf/bootstrap-beans.xml";
+	private final static String LOG4J_URL = "/conf/log4j.properties";
+	public static final String MMS_HOME = "mms.home.dir";
 
-    private static Logger logger = Logger.getLogger(Main.class);
-    
-    public static void main(String[] args) throws Throwable {
-        String homeDir = getHomeDir(args);
-        System.setProperty(MMS_HOME, homeDir);
+	private static int index = 0;
 
-        URL bootURL = getBootURL(args);        
-        Main main = new Main();
-        
-        logger.info("Booting from " + bootURL);
-        main.boot(bootURL);
-        
-        logger.info("Starting main deployer");
-       // main.start();
-    }
+	private Kernel kernel;
+	private BasicXMLDeployer kernelDeployer;
+	private Controller controller;
 
-    /**
-     * Gets the Media Server Home directory.
-     * 
-     * @param args the command line arguments
-     * @return the path to the home directory.
-     */
-    private static String getHomeDir(String args[]) {
-        if (System.getenv(HOME_DIR) == null) {
-            if (args.length > index) {
-                return args[index++];
-            } else {
-                return ".";
-            }
-        } else {
-            return System.getenv(HOME_DIR);
-        }
-    }
+	private static Logger logger = Logger.getLogger(Main.class);
 
-    /**
-     * Gets the URL which points to the boot descriptor.
-     * 
-     * @param args  command line arguments.
-     * @return URL of the boot descriptor.
-     */
-    private static URL getBootURL(String args[]) throws Exception {
-        String bootURL = args.length > index ? args[index] : "${" + MMS_HOME + "}" + BOOT_URL;
-        return getURL(bootURL);
-    }
+	public static void main(String[] args) throws Throwable {
 
-    protected void boot(URL bootURL) throws Throwable {
-        BasicBootstrap bootstrap = new BasicBootstrap();
-        bootstrap.run();
-        
-        registerShutdownThread();
-        
-        kernel = bootstrap.getKernel();
-        kernelDeployer = new BasicXMLDeployer(kernel);
-        
-        kernelDeployer.deploy(bootURL);
-        kernelDeployer.validate();
+		String homeDir = getHomeDir(args);
+		System.setProperty(MMS_HOME, homeDir);
 
-        controller = kernel.getController();
-        start(kernel, kernelDeployer);
-    }
-    
-    public void start(Kernel kernel, BasicXMLDeployer kernelDeployer) {
-        ControllerContext context = controller.getInstalledContext("MainDeployer");
-        if (context != null) {
-            MainDeployer deployer = (MainDeployer) context.getTarget();
-            deployer.start(kernel, kernelDeployer);
-        }
-    }
+		String Log4jURL = homeDir +"/"+ LOG4J_URL;
+		
+		URL log4jurl = getURL(Log4jURL);		
+		InputStream inStreamLog4j = log4jurl.openStream();
+		Properties propertiesLog4j = new Properties();
+		try {
+			propertiesLog4j.load(inStreamLog4j);
+			PropertyConfigurator.configure(propertiesLog4j);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-    public static URL getURL(String url) throws Exception {
-        // replace ${} inputs
-        url = StringPropertyReplacer.replaceProperties(url, System.getProperties());
-        File file = new File(url);
-        if (file.exists() == false) {
-            throw new IllegalArgumentException("No such file: " + url);
-        }
-        return file.toURI().toURL();
-    }
+		logger.info("log4j configured");
+		
+		URL bootURL = getBootURL(args);
+		Main main = new Main();
 
-    protected void registerShutdownThread() {
-        Runtime.getRuntime().addShutdownHook(new Thread(new ShutdownThread()));
-    }
+		logger.info("Booting from " + bootURL);
+		System.out.println("Booting from " + bootURL);
+		main.boot(bootURL);
 
-    private class ShutdownThread implements Runnable {
+		logger.info("Starting main deployer");
+		// main.start();
+	}
 
-        public void run() {
-            System.out.println("Shutting down");
-            kernelDeployer.shutdown();
-            kernelDeployer = null;
+	/**
+	 * Gets the Media Server Home directory.
+	 * 
+	 * @param args
+	 *            the command line arguments
+	 * @return the path to the home directory.
+	 */
+	private static String getHomeDir(String args[]) {
+		if (System.getenv(HOME_DIR) == null) {
+			if (args.length > index) {
+				return args[index++];
+			} else {
+				return ".";
+			}
+		} else {
+			return System.getenv(HOME_DIR);
+		}
+	}
 
-            kernel.getController().shutdown();
-            kernel = null;
-        }
-    }
+	/**
+	 * Gets the URL which points to the boot descriptor.
+	 * 
+	 * @param args
+	 *            command line arguments.
+	 * @return URL of the boot descriptor.
+	 */
+	private static URL getBootURL(String args[]) throws Exception {
+		String bootURL = args.length > index ? args[index] : "${" + MMS_HOME + "}" + BOOT_URL;
+		return getURL(bootURL);
+	}
+
+	protected void boot(URL bootURL) throws Throwable {
+		BasicBootstrap bootstrap = new BasicBootstrap();
+		bootstrap.run();
+
+		registerShutdownThread();
+
+		kernel = bootstrap.getKernel();
+		kernelDeployer = new BasicXMLDeployer(kernel);
+
+		kernelDeployer.deploy(bootURL);
+		kernelDeployer.validate();
+
+		controller = kernel.getController();
+		start(kernel, kernelDeployer);
+	}
+
+	public void start(Kernel kernel, BasicXMLDeployer kernelDeployer) {
+		ControllerContext context = controller.getInstalledContext("MainDeployer");
+		if (context != null) {
+			MainDeployer deployer = (MainDeployer) context.getTarget();
+			deployer.start(kernel, kernelDeployer);
+		}
+	}
+
+	public static URL getURL(String url) throws Exception {
+		// replace ${} inputs
+		url = StringPropertyReplacer.replaceProperties(url, System.getProperties());
+		File file = new File(url);
+		if (file.exists() == false) {
+			throw new IllegalArgumentException("No such file: " + url);
+		}
+		return file.toURI().toURL();
+	}
+
+	protected void registerShutdownThread() {
+		Runtime.getRuntime().addShutdownHook(new Thread(new ShutdownThread()));
+	}
+
+	private class ShutdownThread implements Runnable {
+
+		public void run() {
+			System.out.println("Shutting down");
+			kernelDeployer.shutdown();
+			kernelDeployer = null;
+
+			kernel.getController().shutdown();
+			kernel = null;
+		}
+	}
 }
