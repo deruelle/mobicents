dcee55550affa3cafe5571799a1c53874496e8dd
amit.bhayani@gmail.com <amit.bhayani@gmail.com@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2009-06-01 09:30:02 +0000

    Removed DTMF generation code
    git-svn-id: http://mobicents.googlecode.com/svn/trunk@6248 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- servers/media/core/server-impl/src/test/java/org/mobicents/media/server/impl/rtp/JitterBufferTest.java
+++ servers/media/core/server-impl/src/test/java/org/mobicents/media/server/impl/rtp/JitterBufferTest.java
@@ -3,9 +3,7 @@ package org.mobicents.media.server.impl.rtp;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.concurrent.Semaphore;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -15,7 +13,6 @@ import org.junit.Test;
 import org.mobicents.media.Buffer;
 import org.mobicents.media.Format;
 import org.mobicents.media.server.impl.rtp.sdp.AVProfile;
-import org.mobicents.media.server.spi.resource.DtmfDetector;
 
 /**
  * 
@@ -27,11 +24,8 @@ public class JitterBufferTest {
 	private int period = 20;
 	private int jitter = 40;
 	private JitterBuffer jitterBuffer;
-	private BufferFactory bufferFactory = new BufferFactory(10, "JitterBufferTest");
-	private ArrayList packets = new ArrayList();
 	private final long ssrc = System.currentTimeMillis();
 
-	private Semaphore semaphore;
 
 	@BeforeClass
 	public static void setUpClass() throws Exception {
@@ -48,7 +42,6 @@ public class JitterBufferTest {
 		rtpMap.put(101, AVProfile.DTMF);
 		jitterBuffer = new JitterBuffer(jitter, period, rtpMap);
 
-		semaphore = new Semaphore(0);
 	}
 
 	@After
@@ -63,20 +56,6 @@ public class JitterBufferTest {
 		byte[] res = new byte[172];
 		System.arraycopy(h, 0, res, 0, 12);
 		return res;
-	}
-
-	private byte[] createBuffer(Buffer buffer) {
-		RtpHeader h = (RtpHeader) buffer.getHeader();
-		byte[] headerByte = h.toByteArray();
-
-		int len = headerByte.length + buffer.getLength();
-
-		byte[] senderBuffer = new byte[len];
-
-		// combine RTP header and payload
-		System.arraycopy(headerByte, 0, senderBuffer, 0, headerByte.length);
-		System.arraycopy((byte[]) buffer.getData(), 0, senderBuffer, headerByte.length, buffer.getLength());
-		return senderBuffer;
 	}
 
 	@Test
@@ -99,81 +78,42 @@ public class JitterBufferTest {
 
 	}
 
-	private Buffer generateRfc2833Buffer(boolean endOfEvent, boolean marker, int seq, byte digit, int volume) {
-		Buffer buffer = bufferFactory.allocate();
-
-		RtpHeader rtpHeader = new RtpHeader();
-		rtpHeader.init(marker, (byte) 101, seq, (int) seq * 20, ssrc);
-		buffer.setHeader(rtpHeader);
-
-		byte[] data = (byte[]) buffer.getData();
-		data[0] = digit;
-		data[1] = endOfEvent ? (byte) (volume | 0x80) : (byte) (volume & 0x7f);
-
-		buffer.setOffset(0);
-		buffer.setLength(4);
-		buffer.setFormat(AVProfile.DTMF);
-		buffer.setSequenceNumber(seq);
-		buffer.setTimeStamp(seq * 20);
-
-		return buffer;
-
-	}
-
 	@Test
 	public void testRFC2833Reading() throws Exception {
 
-		int seq = 0;
+		// RFC2833 DtmfEvent for 9, marker=true, endOfEvent = false, volume =
+		// 10, seq = 0, ts = 0
+		byte[] by0 = new byte[] { -128, -27, 0, 0, 0, 0, 0, 0, -101, 29, -126, 82, 9, 10, 0, 0 };
+		// RFC2833 DtmfEvent for 9, marker=false, endOfEvent = false, volume =
+		// 10, seq = 1, ts = 20
+		byte[] by1 = new byte[] { -128, 101, 0, 1, 0, 0, 0, 20, -101, 29, -126, 82, 9, 10, 0, 0 };
 
-		byte digit = (byte) 9;
-		int volume = 10;
-		boolean endOfEvent = false;
+		// RFC2833 DtmfEvent for 9, marker=false, endOfEvent = false, volume =
+		// 10, seq = 2, ts = 40
+		byte[] by2 = new byte[] { -128, 101, 0, 2, 0, 0, 0, 40, -101, 29, -126, 82, 9, 10, 0, 0 };
 
-		Buffer b0 = generateRfc2833Buffer(endOfEvent, true, seq, digit, volume);
-		seq++;
-		Buffer b1 = generateRfc2833Buffer(endOfEvent, false, seq, digit, volume);
-		seq++;
-		Buffer b2 = generateRfc2833Buffer(endOfEvent, false, seq, digit, volume);
-		seq++;
-
-		write(jitterBuffer, createBuffer(b0), 16);
-		write(jitterBuffer, createBuffer(b1), 16);
-		write(jitterBuffer, createBuffer(b2), 16);
+		// RFC2833 DtmfEvent for 9, marker=false, endOfEvent = true, volume =
+		// 10, seq = 3, ts = 60
+		byte[] by3 = new byte[] { -128, 101, 0, 3, 0, 0, 0, 60, -101, 32, -51, -92, 9, -118, 0, 0 };
+		write(jitterBuffer, by0, 16);
+		write(jitterBuffer, by1, 16);
+		write(jitterBuffer, by2, 16);
 
 		Buffer buff = jitterBuffer.read();
 		assertTrue(buff != null);
+		assertEquals(AVProfile.DTMF, buff.getFormat());
 
-		RtpHeader head = (RtpHeader) buff.getHeader();
-		assertTrue(head.getMarker());
-		byte[] data = (byte[]) buff.getData();
+		write(jitterBuffer, by3, 16);
 
-		String recdDigit = DtmfDetector.TONE[data[0]];
-		assertEquals("9", recdDigit);
-		boolean end = (data[1] & 0x80) != 0;
-		assertEquals(false, end);
-		
-		endOfEvent = true;
-		Buffer b3 = generateRfc2833Buffer(endOfEvent, false, seq, digit, volume);
-		seq++;
-		write(jitterBuffer, createBuffer(b3), 16);
-		
 		buff = jitterBuffer.read();
 		assertTrue(buff != null);
-		
-		buff = jitterBuffer.read();
-		assertTrue(buff != null);
-		
-		buff = jitterBuffer.read();
-		assertTrue(buff != null);
-		
-		head = (RtpHeader) buff.getHeader();
-		assertTrue(!head.getMarker());
-		data = (byte[]) buff.getData();
 
-		recdDigit = DtmfDetector.TONE[data[0]];
-		assertEquals("9", recdDigit);
-		end = (data[1] & 0x80) != 0;
-		assertEquals(true, end);
+		buff = jitterBuffer.read();
+		assertTrue(buff != null);
+
+		buff = jitterBuffer.read();
+		assertTrue(buff != null);
+		assertEquals(AVProfile.DTMF, buff.getFormat());
 
 	}
 
