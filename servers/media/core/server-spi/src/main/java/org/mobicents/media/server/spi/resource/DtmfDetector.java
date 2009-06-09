0caec61a326b9210340f82cd738160f88c9a6c75
oleg.kulikoff <oleg.kulikoff@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2009-06-09 12:39:04 +0000

    Fix: Detection problem
    git-svn-id: http://mobicents.googlecode.com/svn/trunk@6492 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- servers/media/core/server-spi/src/main/java/org/mobicents/media/server/spi/resource/DtmfDetector.java
+++ servers/media/core/server-spi/src/main/java/org/mobicents/media/server/spi/resource/DtmfDetector.java
@@ -1,8 +1,6 @@
 package org.mobicents.media.server.spi.resource;
 
-import org.mobicents.media.Format;
 import org.mobicents.media.MediaSink;
-import org.mobicents.media.format.AudioFormat;
 
 /**
  * 
@@ -26,12 +24,6 @@ public interface DtmfDetector extends MediaSink {
 
 	public void setMask(String mask);
 
-	public void setSilenec(int silence);
 
-	public int getSilenec();
-
-	public void setDuration(int duartion);
-
-	public int getDuration();
 
 }
