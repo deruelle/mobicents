972b5501e0220e8764458cb14fccf86647acf916
amit.bhayani@gmail.com <amit.bhayani@gmail.com@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2009-08-07 07:48:22 +0000

    Added test for silence detection even if buffer.eom is true
    git-svn-id: http://mobicents.googlecode.com/svn/trunk@7393 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- servers/media/core/server-impl/src/test/java/org/mobicents/media/server/impl/resource/audio/EndpointTransmittorTest.java
+++ servers/media/core/server-impl/src/test/java/org/mobicents/media/server/impl/resource/audio/EndpointTransmittorTest.java
@@ -20,160 +20,232 @@ import org.mobicents.media.Format;
 import org.mobicents.media.server.impl.AbstractSink;
 import org.mobicents.media.server.impl.clock.TimerImpl;
 import org.mobicents.media.server.impl.resource.test.SineGenerator;
+import org.mobicents.media.server.spi.Endpoint;
 import org.mobicents.media.server.spi.Timer;
 import org.mobicents.media.server.spi.dsp.Codec;
 
 /**
- *
+ * 
  * @author kulikov
  */
 public class EndpointTransmittorTest {
 
-    private final static int F = 50;
-    private Timer timer;
-    
-    private EndpointTransmittor filler;
-    private SineGenerator gen;
-    private TestSink sink;
-    
-    private Semaphore semaphore = new Semaphore(0);
-   
-    private ArrayList<double[]> s = new ArrayList();
-    private long maxDiff;
-    private boolean signalDetected = true;
-    private boolean outOfSeq;
-    private int count = 0;
-    private long wrongSeq;
-    private long maxDifNo;
-    
-    private boolean silenceExpected = false;
-    
-    public EndpointTransmittorTest() {
-    }
+	private final static int F = 50;
+	private Timer timer;
 
-    @BeforeClass
-    public static void setUpClass() throws Exception {
-    }
+	private EndpointTransmittor filler;
+	private SineGenerator gen;
+	private ExtSineGenerator extgen;
+	private TestSink sink;
 
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-    }
+	private Semaphore semaphore = new Semaphore(0);
 
-    @Before
-    public void setUp() {
-        timer = new TimerImpl();
-        
-        gen = new SineGenerator("test.sine", timer); 
-        gen.setAmplitude(Short.MAX_VALUE);
-        gen.setFrequency(F);
-        
-        sink = new TestSink("test.sink");
-        filler = new EndpointTransmittor("test.filler", timer);
-        
-        gen.connect(filler.getInput());
-        sink.connect(filler.getOutput());
-    }
+	private ArrayList<double[]> s = new ArrayList();
+	private long maxDiff;
+	private boolean signalDetected = true;
+	private boolean outOfSeq;
+	private int count = 0;
+	private long wrongSeq;
+	private long maxDifNo;
 
-    @After
-    public void tearDown() {
-    }
+	private boolean silenceExpected = false;
 
-    /**
-     * Test of getInput method, of class NoiseFiller.
-     */
-    @Test
-    public void testTransmission() throws Exception {
-        filler.getInput().start();
-        filler.getOutput().start();
-        
-        gen.start();
-        sink.start();
-        
-        semaphore.tryAcquire(5, TimeUnit.SECONDS);
-        gen.stop();
-        
-        
-        assertTrue("Interarrival interval to long", maxDiff < 60);
-        assertTrue("Signal not detected", signalDetected);
-        assertFalse("Out of sequence", outOfSeq);
-        assertTrue("Signal too short", count > 200);
-        
-        signalDetected = false;
-        count = 0;
- 
-        this.silenceExpected = true;
-        
-        semaphore.tryAcquire(5, TimeUnit.SECONDS);
-        
-        assertTrue("Interarrival interval to long", maxDiff < 60);
-        assertFalse("Signal detected", signalDetected);
-        assertFalse("Out of sequence", outOfSeq);
-        assertTrue("Signal too short", count > 200);
-  
-    }
+	public EndpointTransmittorTest() {
+	}
 
-    private class TestSink extends AbstractSink {
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+	}
 
-        private long timestamp;
-        private long seq;
-        
-        public TestSink(String name) {
-            super(name);
-        }
-        
-        @Override
-        public void onMediaTransfer(Buffer buffer) throws IOException {  
-            count++;
-            long next = buffer.getTimeStamp();
-            long seqNext = buffer.getSequenceNumber();
-            
-            if (count == 1) {
-                timestamp = next;
-                seq = seqNext;
-                return;
-            }
-            
-            long diff = Math.abs(next - timestamp);
-            if (diff > maxDiff && !silenceExpected) {
-                maxDiff = diff;
-                maxDifNo = seqNext;
-            } 
-            
-            if (silenceExpected) {
-                silenceExpected = false;
-            }
-            
-            if (seqNext - seq != 1) {
-                outOfSeq = true;
-                wrongSeq = seq;
-            }
-            
-            timestamp = next;
-            seq = seqNext;
-            
-            byte[] data = (byte[]) buffer.getData();
-            short[] signal = new short[buffer.getLength() /2];
-            int k = buffer.getOffset();
-            for (int i = 0; i < signal.length; i++) {
-                 signal[i] = (short)((data[k++] & 0xff) | (data[k++] << 8));
-            }
-            
-            for (int i = 0; i < signal.length; i++) {
-                if (signal[i] > 320) {
-                    signalDetected = true;
-                    return;
-                }
-            }
-            
-        }
+	@AfterClass
+	public static void tearDownClass() throws Exception {
+	}
 
-        public Format[] getFormats() {
-            return new Format[] {Codec.LINEAR_AUDIO};
-        }
+	@Before
+	public void setUp() {
+		timer = new TimerImpl();
 
-        public boolean isAcceptable(Format format) {
-            return Codec.LINEAR_AUDIO.matches(format);
-        }
-        
-    }
+		gen = new SineGenerator("test.sine", timer);
+		gen.setAmplitude(Short.MAX_VALUE);
+		gen.setFrequency(F);
+
+		extgen = new ExtSineGenerator("test.sine", timer);
+		extgen.setAmplitude(Short.MAX_VALUE);
+		extgen.setFrequency(F);
+		extgen.setGenerationTime(3000);
+
+		sink = new TestSink("test.sink");
+		filler = new EndpointTransmittor("test.filler", timer);
+
+	}
+
+	@After
+	public void tearDown() {
+	}
+
+	/**
+	 * Test of getInput method, of class NoiseFiller.
+	 */
+	@Test
+	public void testTransmission() throws Exception {
+		gen.connect(filler.getInput());
+		sink.connect(filler.getOutput());
+
+		filler.getInput().start();
+		filler.getOutput().start();
+
+		gen.start();
+		sink.start();
+
+		semaphore.tryAcquire(5, TimeUnit.SECONDS);
+		gen.stop();
+
+		assertTrue("Interarrival interval to long", maxDiff < 60);
+		assertTrue("Signal not detected", signalDetected);
+		assertFalse("Out of sequence", outOfSeq);
+		assertTrue("Signal too short", count > 200);
+
+		signalDetected = false;
+		count = 0;
+
+		this.silenceExpected = true;
+
+		semaphore.tryAcquire(5, TimeUnit.SECONDS);
+
+		assertTrue("Interarrival interval to long", maxDiff < 60);
+		assertFalse("Signal detected", signalDetected);
+		assertFalse("Out of sequence", outOfSeq);
+		assertTrue("Signal too short", count > 200);
+
+	}
+
+	/**
+	 * Test of getInput method, of class NoiseFiller.
+	 */
+
+	@Test
+	public void testTransmission2() throws Exception {
+
+		extgen.connect(filler.getInput());
+		sink.connect(filler.getOutput());
+
+		filler.getInput().start();
+		filler.getOutput().start();
+
+		extgen.start();
+		sink.start();
+
+		semaphore.tryAcquire(3, TimeUnit.SECONDS);
+		extgen.stop();
+
+		assertTrue("Interarrival interval to long", maxDiff < 60);
+		assertTrue("Signal not detected", signalDetected);
+		assertFalse("Out of sequence", outOfSeq);
+		assertTrue("Signal too short", count > 140);
+
+		signalDetected = false;
+		count = 0;
+
+		this.silenceExpected = true;
+
+		semaphore.tryAcquire(5, TimeUnit.SECONDS);
+
+		assertTrue("Interarrival interval to long", maxDiff < 60);
+		assertFalse("Signal detected", signalDetected);
+		assertFalse("Out of sequence", outOfSeq);
+		
+		assertTrue("Signal too short", count > 200);
+	}
+
+	private class TestSink extends AbstractSink {
+
+		private long timestamp;
+		private long seq;
+
+		public TestSink(String name) {
+			super(name);
+		}
+
+		@Override
+		public void onMediaTransfer(Buffer buffer) throws IOException {
+			count++;
+			long next = buffer.getTimeStamp();
+			long seqNext = buffer.getSequenceNumber();
+
+			if (count == 1) {
+				timestamp = next;
+				seq = seqNext;
+				return;
+			}
+
+			long diff = Math.abs(next - timestamp);
+			if (diff > maxDiff && !silenceExpected) {
+				maxDiff = diff;
+				maxDifNo = seqNext;
+			}
+
+			if (silenceExpected) {
+				silenceExpected = false;
+			}
+
+			if (seqNext - seq != 1) {
+				outOfSeq = true;
+				wrongSeq = seq;
+			}
+
+			timestamp = next;
+			seq = seqNext;
+
+			byte[] data = (byte[]) buffer.getData();
+			short[] signal = new short[buffer.getLength() / 2];
+			int k = buffer.getOffset();
+			for (int i = 0; i < signal.length; i++) {
+				signal[i] = (short) ((data[k++] & 0xff) | (data[k++] << 8));
+			}
+
+			for (int i = 0; i < signal.length; i++) {
+				if (signal[i] > 320) {
+					signalDetected = true;
+					return;
+				}
+			}
+
+		}
+
+		public Format[] getFormats() {
+			return new Format[] { Codec.LINEAR_AUDIO };
+		}
+
+		public boolean isAcceptable(Format format) {
+			return Codec.LINEAR_AUDIO.matches(format);
+		}
+
+	}
+
+	private class ExtSineGenerator extends SineGenerator {
+
+		private double time;
+
+		private double generationTime;
+
+		public ExtSineGenerator(String name, Timer timer) {
+			super(name, timer);
+		}
+
+		public void setGenerationTime(double generationTime) {
+			this.generationTime = generationTime;
+
+		}
+
+		@Override
+		public void evolve(Buffer buffer, long seq) {
+			super.evolve(buffer, seq);
+			time += ((double) getSyncSource().getHeartBeat()) / 1000.0;
+
+			if (time >= (this.generationTime / 1000.0)) {
+				buffer.setEOM(true);
+			}
+		}
+
+	}
 }
