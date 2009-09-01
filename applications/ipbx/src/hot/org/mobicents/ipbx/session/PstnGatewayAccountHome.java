30d96fa13b6da38b225eb2cf67237dc46cbdd8c1
vladimir.ralev <vladimir.ralev@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2009-09-01 17:45:36 +0000

    Dynamic update of PSTN accounts
    git-svn-id: http://mobicents.googlecode.com/svn/trunk@7513 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- applications/ipbx/src/hot/org/mobicents/ipbx/session/PstnGatewayAccountHome.java
+++ applications/ipbx/src/hot/org/mobicents/ipbx/session/PstnGatewayAccountHome.java
@@ -3,6 +3,7 @@ package org.mobicents.ipbx.session;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Begin;
 import org.jboss.seam.annotations.web.RequestParameter;
+import org.jboss.seam.core.Events;
 import org.jboss.seam.framework.EntityHome;
 
 import org.mobicents.ipbx.entity.PstnGatewayAccount;
@@ -30,5 +31,19 @@ public class PstnGatewayAccountHome extends EntityHome<PstnGatewayAccount>
     public void create() {
         super.create();
     }
+    
+    @Override
+	public String persist() {
+		String ret = super.persist();
+		Events.instance().raiseEvent("globalSettingsChanged");
+		return ret;
+	}
+
+	@Override
+	public String update() {
+		String ret = super.update();
+		Events.instance().raiseEvent("globalSettingsChanged");
+		return ret;
+	}
 
 }
